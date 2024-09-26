5050cc545d74a6ee8b4e1b10bff7f9009fb1a5a7
yoelvp <yoelvp73@gmail.com>
2024-09-25 21:31:45 -0500

    style: change `key` to string

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,22 +3,24 @@ import flowbite from 'flowbite-react/tailwind'
 
 const config: Config = {
   content: [
+    './src/@common/**/*.{ts,tsx}',
+    './src/@features/**/*.{ts,tsx}',
     './src/modules/**/*.{ts,tsx}',
-    './src/@common/**/*.{ts,tsx}'
+    './src/layouts/**/*.{ts,tsx}'
   ],
   theme: {
     extend: {
       zIndex: {
-        100: '100',
-        200: '200',
-        300: '300',
-        400: '400',
-        500: '500',
-        600: '600',
-        700: '700',
-        800: '800',
-        900: '900',
-        1000: '1000'
+        '100': '100',
+        '200': '200',
+        '300': '300',
+        '400': '400',
+        '500': '500',
+        '600': '600',
+        '700': '700',
+        '800': '800',
+        '900': '900',
+        '1000': '1000'
       }
     },
     colors: {
