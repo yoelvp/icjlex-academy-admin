54b1a047318d1a16d3c95760eb55d2e48fdc8410
yoelvp <yoelvp73@gmail.com>
2024-09-25 21:25:39 -0500

    fix: add new const

--- src/@common/env.ts
+++ src/@common/env.ts
@@ -1,2 +1,8 @@
+// Env variables
 export const API_URL = import.meta.env.VITE_BASE_URL_API_V1
 export const JWT_SECRET_KEY = import.meta.env.JWT_SECRET_KEY
+
+// Utilities
+export const APP_NAME = 'ICJ LEX & CARRANZA CONSULTORES'
+export const APP_SHORT_NAME= 'ICJ LEX'
+export const WHATSAPP_LINK = 'https://wa.me/51942208501'
