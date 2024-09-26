0be56e414a74db0393a96ba83b3558c18c2b5ffa
yoelvp <yoelvp73@gmail.com>
2024-09-26 00:02:24 -0500

    fix: change environment variable

--- src/@common/env.ts
+++ src/@common/env.ts
@@ -1,2 +1,2 @@
 export const API_URL = import.meta.env.VITE_BASE_URL_API_V1
-export const JWT_SECRET_KEY = import.meta.env.JWT_SECRET_KEY
+export const JWT_SECRET_KEY = import.meta.env.VITE_JWT_SECRET_KEY
