92e7dd8129491963414eea7d983e2f0ca8d9e31d
yoelvp <yoelvp73@gmail.com>
2024-09-25 21:29:41 -0500

    fix: change layout

--- src/layouts/admin/index.tsx
+++ src/layouts/admin/index.tsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import classNames from 'classnames'
 import { Sidebar } from '@/@features/admin/components/sidebar'
 import { Header } from '@/@features/admin/components/header'
+import Content from '@/@features/admin/components/content'
+import { useSidebar } from '@/@features/admin/store/use-sidebar.store'
 
 export const AdminLayout = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen)
-  }
+  const show = useSidebar((state) => state.show)
 
   return (
-    <div className="relative w-full flex">
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <div className="w-full h-screen">
-        <Header toggleSidebar={toggleSidebar} />
-        <Outlet />
+    <div className="h-screen flex">
+      <Sidebar />
+
+      <div className={classNames('flex flex-1 flex-col overflow-hidden', { 'ml-64': show, 'ml-16': !show })}>
+        <Header />
+
+        <div className="flex-1 overflow-y-scroll w-full">
+          <Content>
+            <Outlet />
+          </Content>
+        </div>
       </div>
     </div>
   )
