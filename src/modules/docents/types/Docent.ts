d72c6d2b2c8dbf23d32cec693496790c1aecd9b8
valdir169 <contrerasvaldir06@gmail.com>
2024-09-28 12:21:25 -0500

    feat: configure the state global with suztand

---
+++ src/modules/docents/types/Docent.ts
@@ -0,0 +1,28 @@
+import { InferType } from 'yup'
+import { docentSchema } from '../schemas/docent.schema'
+
+export type Docent = InferType<typeof docentSchema>
+
+export type DocentResult = {
+  id: string
+  firstName: string
+  lastName: string
+  profession: string
+  aboutMe: string
+  valorations: number
+  imageUrl: null | string
+  socialMedia: SocialMediaClass | null
+  docentToSpecialty: DocentToSpecialty[]
+}
+
+export interface DocentToSpecialty {
+  specialtyName: string
+}
+
+export interface SocialMediaClass {
+  whatsapp: string
+  x: string
+  facebook: string
+  linkedin: string
+  youtube: string
+}
