ded8b9704ad19af90cb26c74cc344ca2f698ecf8
valdir169 <contrerasvaldir06@gmail.com>
2024-10-06 16:18:04 -0500

    fix: change props of button action

--- src/@common/components/button-action.tsx
+++ src/@common/components/button-action.tsx
@@ -1,22 +1,26 @@
 import { ElementType } from 'react'
 
 interface Props {
-  text: string
+  label: string
   icon: ElementType
-  disabled?: boolean
+  className?: string
   onClick: () => void
 }
 
-export const ButtonAction = ({ text, icon: Icon, disabled, onClick }: Props) => {
+export const ButtonAction = ({
+  label,
+  icon: Icon,
+  className,
+  onClick
+}: Props) => {
   return (
     <button
       className={`flex items-center w-full gap-2 py-3 px-4 rounded-sm transition duration-200 text-primary-500 hover:bg-primary-400 hover:text-primary-50 
-      ${disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}`}
+      ${className}`}
       onClick={onClick}
-      disabled={disabled}
     >
       <Icon size={24} />
-      {text}
+      {label}
     </button>
   )
 }
