284c21518c11efe7e459a8de9ecf90d0fcec468e
valdir169 <contrerasvaldir06@gmail.com>
2024-10-06 18:25:18 -0500

    feat: validate course schema

--- src/modules/courses/schemas/course.schema.tsx
+++ src/modules/courses/schemas/course.schema.tsx
@@ -1,8 +1,19 @@
-import { boolean, object, string } from 'yup'
+import { mixed, object, string } from 'yup'
 
 export const courseSchema = object({
   id: string().optional(),
   name: string().required('Este es un campo requerido'),
-  description: string().required('Este es un campo requerido'),
-  isActive: boolean().required('Este es un campo rquerido')
+  objetive: string().required('Este es un campo requerido'),
+  image: mixed()
+    .required('La imagen es obligatoria.')
+    .test('fileSize', 'La imagen es muy grande.', (value) => {
+      // Limitar a 2MB
+      return value && value[0]?.size <= 2 * 1024 * 1024
+    })
+    .test('fileType', 'El formato de archivo no es válido.', (value) => {
+      return (
+        value &&
+        ['image/jpeg', 'image/png', 'image/gif'].includes(value[0]?.type)
+      )
+    })
 })
