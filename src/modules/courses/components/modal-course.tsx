170e6bd48d37ce6e70c16926df1b5b6587303de6
valdir169 <contrerasvaldir06@gmail.com>
2024-10-10 20:57:23 -0500

    feat: add form multi step

--- src/modules/courses/components/modal-course.tsx
+++ src/modules/courses/components/modal-course.tsx
@@ -1,11 +1,20 @@
 import { FC } from 'react'
 
-import Button from '@/@common/components/button'
-import Form from '@/@common/components/form'
+import { useState } from 'react'
+
 import { Modal } from '@/modules/dashboard/components/modal'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form'
 import { courseSchema } from '../schemas/course.schema'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useDocentStore } from '@/modules/teachers/store/teachers.store'
+import { useCreateCourse } from '../hooks/use-create-courses'
+import { Course } from '../types/Course'
+import { useGetAllTeachers } from '@/modules/teachers/hooks/use-get-all-teachers'
+
+import Form from '@/@common/components/form'
+import Button from '@/@common/components/button'
+import ImageUploader from './image-uploader'
+import { STEPS } from '../utils/constants'
 
 interface ModalCourseProps {
   isOpen: boolean
@@ -13,74 +22,267 @@ interface ModalCourseProps {
 }
 
 const ModalCourse: FC<ModalCourseProps> = ({ isOpen, onClose }) => {
+  const [step, setStep] = useState(0)
+  const { isLoading: loadingTeacher } = useGetAllTeachers()
+  const teachers = useDocentStore((state) => state.teachers)
+  const { createCourse, isLoading } = useCreateCourse()
+
   const {
     register,
     handleSubmit,
+    control,
+    setValue,
     formState: { errors, isDirty },
     reset
-  } = useForm({
+  } = useForm<Course>({
     resolver: yupResolver(courseSchema),
     defaultValues: {
       name: '',
       objetive: '',
-      image: ''
+      docentId: '',
+      content: [
+        { title: '', details: [{ titleClass: '', duration: '', videoUrl: '' }] }
+      ]
     }
   })
 
-  const onSubmit: SubmitHandler<T> = (data) => {
-    console.log(data)
+  const { fields: contentFields } = useFieldArray({
+    control,
+    name: 'content'
+  })
+
+  const { fields: detailsFields } = useFieldArray({
+    control,
+    name: 'content.0.details'
+  })
+
+  const onSubmit: SubmitHandler<Course> = (data) => {
+    createCourse(data)
     reset()
+    onClose()
   }
+
+  const nextStep = () => setStep((prev) => Math.min(prev + 1, STEPS.length - 1))
+  const prevStep = () => setStep((prev) => Math.max(prev - 1, 0))
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} title="Agregar Curso">
-      <Form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-y-4 w-full  overflow-y-auto"
-        autoComplete="off"
-      >
-        <Form.Control>
-          <Form.Label>Nombre</Form.Label>
-          <Form.Input
-            placeholder="Ingresa el nombre..."
-            size="md"
-            {...register('name')}
-          />
-          <Form.Error hasError={errors.name?.message} />
-        </Form.Control>
+      <div className="mb-4">
+        <div className="flex justify-between mb-2">
+          {STEPS.map((s, index) => (
+            <div
+              key={index}
+              className={`w-1/4 text-center ${
+                index <= step ? 'text-primary-400' : 'text-gray-400'
+              }`}
+            >
+              <div
+                className={`w-8 h-8 mx-auto rounded-full flex items-center justify-center ${
+                  index <= step
+                    ? 'bg-secondary-500 text-primary-500'
+                    : 'bg-gray-200'
+                }`}
+              >
+                {index + 1}
+              </div>
+              <div className="mt-2 text-sm">{s.title}</div>
+            </div>
+          ))}
+        </div>
+        <div className="relative pt-1">
+          <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-primary-200">
+            <div
+              style={{ width: `${((step + 1) / STEPS.length) * 100}%` }}
+              className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-primary-600"
+            ></div>
+          </div>
+        </div>
+      </div>
 
-        <Form.Control>
-          <Form.Label>Objetivo</Form.Label>
-          <Form.Input
-            placeholder="Ingresa el objetivo..."
-            size="md"
-            {...register('objetive')}
-          />
-          <Form.Error hasError={errors.objetive?.message} />
-        </Form.Control>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        {step === 0 && (
+          <div className="space-y-4">
+            <Form.Control>
+              <Form.Label>Nombre del curso</Form.Label>
+              <Form.Input
+                placeholder="Violencia contra Niñas, Niños y Adolescente en el ámbito..."
+                size="md"
+                {...register('name')}
+              />
+              <Form.Error hasError={errors.name?.message} />
+            </Form.Control>
 
-        <Form.Control>
-          <Form.Label>Imagen</Form.Label>
-          <input
-            type="file"
-            accept="image/*"
-            className="block w-full text-sm text-primary-500 border border-primary-400 rounded-lg cursor-pointer  focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            {...register('image')}
-          />
-          <Form.Error hasError={errors.image?.message} />
-        </Form.Control>
+            <Form.Control>
+              <Form.Label>Objetivo</Form.Label>
+              <Form.Input
+                placeholder="El curso tiene como objetivo enseñar a los estudiantes..."
+                size="md"
+                {...register('objetive')}
+              />
+              <Form.Error hasError={errors.objetive?.message} />
+            </Form.Control>
 
-        <div className="flex gap-8 w-full mt-8">
+            <Form.Control>
+              <Form.Label>Docente</Form.Label>
+              <select
+                className="text-sm text-primary-500 border border-primary-400 rounded-lg "
+                {...register('docentId')}
+              >
+                <option value="">
+                  {loadingTeacher
+                    ? 'Cargando docentes...'
+                    : 'Selecciona un docente'}
+                </option>
+
+                {!loadingTeacher &&
+                  teachers.map((teacher) => (
+                    <option
+                      key={teacher.id}
+                      className="text-sm text-primary-500 border border-primary-400 rounded-lg"
+                      value={teacher.id}
+                    >
+                      {teacher.firstName} {teacher.lastName}
+                    </option>
+                  ))}
+              </select>
+              <Form.Error hasError={errors.docentId?.message} />
+            </Form.Control>
+          </div>
+        )}
+        {step === 1 && (
+          <div className="space-y-6 w-full h-full">
+            {contentFields.map((field, index) => (
+              <div
+                key={field.id}
+                className="p-4 border border-primary-100 rounded-xs"
+              >
+                <Form.Control>
+                  <Form.Label
+                    htmlFor={`content.${index}.title`}
+                    className="font-bold"
+                  >
+                    Título del Contenido
+                  </Form.Label>
+                  <Form.Input
+                    {...register(`content.${index}.title`)}
+                    id={`content.${index}.title`}
+                    type="text"
+                  />
+                  {errors.content && errors.content[index] && (
+                    <Form.Error
+                      hasError={errors.content[index].title?.message}
+                    />
+                  )}
+                </Form.Control>
+              </div>
+            ))}
+          </div>
+        )}
+
+        {step === 2 && (
+          <div className="space-y-4">
+            {detailsFields.map((field, index) => (
+              <div
+                key={field.id}
+                className="p-4 border border-primary-100 rounded-xs space-y-6"
+              >
+                <Form.Control>
+                  <Form.Label htmlFor={`content.0.details.${index}.titleClass`}>
+                    Título de la Clase
+                  </Form.Label>
+                  <Form.Input
+                    {...register(`content.0.details.${index}.titleClass`)}
+                    id={`content.0.details.${index}.titleClass`}
+                    type="text"
+                  />
+                  {errors.content &&
+                    errors.content[0] &&
+                    errors.content[0].details && (
+                    <Form.Error
+                      hasError={
+                        errors.content[0]?.details?.[index]?.titleClass
+                          ?.message
+                      }
+                    />
+                  )}
+                </Form.Control>
+
+                <Form.Control className="mt-2">
+                  <Form.Label htmlFor={`content.0.details.${index}.duration`}>
+                    Duración
+                  </Form.Label>
+                  <Form.Input
+                    {...register(`content.0.details.${index}.duration`)}
+                    id={`content.0.details.${index}.duration`}
+                    type="text"
+                    placeholder="HH:MM:SS"
+                  />
+                  {errors.content &&
+                    errors.content[0] &&
+                    errors.content[0].details && (
+                    <Form.Error
+                      hasError={
+                        errors.content[0].details?.[index]?.duration?.message
+                      }
+                    />
+                  )}
+                </Form.Control>
+
+                <Form.Control className="mt-2">
+                  <Form.Label htmlFor={`content.0.details.${index}.videoUrl`}>
+                    URL del Video
+                  </Form.Label>
+                  <Form.Input
+                    {...register(`content.0.details.${index}.videoUrl`)}
+                    id={`content.0.details.${index}.videoUrl`}
+                    type="text"
+                  />
+                  {errors.content &&
+                    errors.content[0] &&
+                    errors.content[0].details && (
+                    <Form.Error
+                      hasError={
+                        errors.content?.[0].details?.[index]?.videoUrl
+                          ?.message
+                      }
+                    />
+                  )}
+                </Form.Control>
+              </div>
+            ))}
+          </div>
+        )}
+
+        {step === 3 && (
+          <div className="space-y-4 p-4 border border-primary-100 rounded-xs">
+            <Form.Control>
+              <Form.Label className="">
+                Selecciona la imagen del curso
+              </Form.Label>
+              <ImageUploader name="image" setValue={setValue} />
+              <Form.Error hasError={errors.image?.message} />
+            </Form.Control>
+          </div>
+        )}
+
+        <div className="mt-8 flex justify-between">
           <Button
-            variant="error"
-            className="w-full cursor-pointer"
-            onClick={onClose}
+            type="button"
+            variant={'white'}
+            onClick={prevStep}
+            className={` ${step === 0 ? 'invisible' : ''}`}
           >
-            Cerrar
+            Anterior
           </Button>
-          <Button className="w-full" disabled={!isDirty}>
-            Enviar
-          </Button>
+          {step < STEPS.length - 1 ? (
+            <Button type="button" onClick={nextStep}>
+              Siguiente
+            </Button>
+          ) : (
+            <Button type="submit" disabled={!isDirty}>
+              {isLoading ? 'Creando...' : 'Enviar'}
+            </Button>
+          )}
         </div>
       </Form>
     </Modal>
