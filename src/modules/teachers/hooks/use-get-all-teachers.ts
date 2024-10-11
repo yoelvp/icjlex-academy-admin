63f22ddd0d3686e6f9d557504af2d5cc5719cd31
valdir169 <contrerasvaldir06@gmail.com>
2024-10-10 20:53:37 -0500

    feat: get all teachers without pagination

---
+++ src/modules/teachers/hooks/use-get-all-teachers.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { toast } from 'sonner'
+import { useDocentStore } from '../store/teachers.store'
+import { getAllTeachersService } from '../service/docents.service'
+import getError from '@/@common/utils/get-errors'
+import { useLoading } from '@/@common/hooks/use-loading'
+
+export const useGetAllTeachers = () => {
+  const { isLoading, loading, loaded } = useLoading()
+  const setTeachers = useDocentStore((state) => state.setTeachers)
+
+  useEffect(() => {
+    fetchTeachers()
+  }, [])
+
+  const fetchTeachers = async () => {
+    loading()
+    try {
+      const { data } = await getAllTeachersService(1, 9999, 9999)
+      setTeachers(data.results)
+    } catch (error) {
+      const { message } = getError(error)
+      toast.error(message)
+    } finally {
+      loaded()
+    }
+  }
+
+  return { isLoading }
+}
