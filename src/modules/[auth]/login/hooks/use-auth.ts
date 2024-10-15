ac362e5df1053f3119f7373195d3733592b3049a
yoelvp <yoelvp73@gmail.com>
2024-10-15 16:03:55 -0500

    fix: change access token storage and errors

--- src/modules/[auth]/login/hooks/use-auth.ts
+++ src/modules/[auth]/login/hooks/use-auth.ts
@@ -2,12 +2,15 @@ import type { LoginFormSchema } from '../types/Login'
 
 import { useNavigate } from 'react-router-dom'
 import { jwtVerify } from 'jose'
+import Cookies from 'js-cookie'
+import { toast } from 'sonner'
 import { getUserById, loginService } from '../services/login'
 import { useTokenStore } from '@/@auth/store/use-token.store'
 import { useUserStore } from '@/@auth/store/use-user.store'
 import { JWT_SECRET_KEY } from '@/@common/env'
 import { useLoading } from '@/@common/hooks/use-loading'
 import getError from '@/@common/utils/get-errors'
+import { CookieKeys } from '../utils'
 
 export const useAuth = () => {
   const navigate = useNavigate()
@@ -19,21 +22,25 @@ export const useAuth = () => {
     try {
       loading()
       const { data: loginData } = await loginService(data)
-      const accessToken = loginData.access.token
+      const accessToken = loginData.access
+      const refreshToken = loginData.refresh
 
-      if (!accessToken) return logout()
+      if (!accessToken.token) return logout()
 
-      setToken(accessToken)
+      setToken(accessToken.token)
+      Cookies.set(CookieKeys.TOKEN, accessToken.token, { expires: new Date(accessToken.expires), secure: true })
+      Cookies.set(CookieKeys.REFRESH_TOKEN, refreshToken.token, { expires: new Date(refreshToken.expires) })
 
-      const { payload: { sub: userId } } = await jwtVerify(loginData.access.token, new TextEncoder().encode(JWT_SECRET_KEY))
-      const { data: userData } = await getUserById(userId ?? '')
+      const { payload: { sub: userId } } = await jwtVerify(accessToken.token, new TextEncoder().encode(JWT_SECRET_KEY))
+      const { data: userData, status } = await getUserById(userId ?? '')
 
-      setUser(userData)
-      navigate('/admin')
-    } catch (error: any) {
+      if (status === 200) {
+        setUser(userData)
+        navigate('/admin')
+      }
+    } catch (error) {
       const { message } = getError(error)
-      console.error(error)
-      console.warn(message)
+      toast.error(message)
     } finally {
       loaded()
     }
