52bf98695c5a3a2c737c30c5ecd1969ec50df395
yoelvp <yoelvp73@gmail.com>
2024-10-14 21:26:35 -0500

    fix: add list data and load modal

--- src/modules/students/views/index.tsx
+++ src/modules/students/views/index.tsx
@@ -1,18 +1,23 @@
 import { lazy, Suspense } from 'react'
+import { Spinner, Tabs } from 'flowbite-react'
 import Button from '@/@common/components/button'
 import Form from '@/@common/components/form'
 import { useShow } from '@/@common/hooks/use-show'
+import { useStudentsStore } from '../store/use-students.store'
+import { StudentTab } from '../enums/student-tab'
+import { TableLoading } from '@/@common/components/table-loading'
+import { useStudentsUI } from '../hooks'
+import { useGetActiveStudents, useGetPreRegisteredStudents } from '../hooks'
 
-const d = (promise) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, 5000)
-  }).then(() => promise)
-}
-
-const RegisterStudentModal = lazy(() => d(import('../components/register-student-modal')))
+const RegisterStudentModal = lazy(() => import('../components/register-student-modal'))
 
 const StudentsPage = () => {
   const { show, open, close } = useShow()
+  const activeStudents = useStudentsStore((state) => state.activeStudents)
+  const preRegisteredStudents = useStudentsStore((state) => state.preRegisteredStudents)
+  const { tab, handleTabIndex } = useStudentsUI()
+  const { isLoading: isLoadingActive } = useGetActiveStudents({ page: 1, size: 10 })
+  const { isLoading } = useGetPreRegisteredStudents({ page: 1, size: 10 })
 
   return (
     <div className="flex flex-col gap-y-8">
@@ -24,60 +29,107 @@ const StudentsPage = () => {
         <div className="flex gap-x-2">
           <Form.Input type="search" size="md" placeholder="Buscar estudiante..." />
           <Button size="sm" onClick={open}>
+            {show && (
+              <Suspense fallback={<Spinner size="sm" />}>
+                <RegisterStudentModal isOpen={show} onClose={close} />
+              </Suspense>
+            )}
             Crear
           </Button>
         </div>
       </header>
 
       <section className="section-panel p-4">
-        <table className="custom-table">
-          <thead>
-            <tr>
-              <th>
-                Nombres
-              </th>
-              <th>
-                Email
-              </th>
-              <th>
-                N° teléfono
-              </th>
-              <th>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                Manuel Esquivel
-              </td>
-              <td>
-                manuel@gmail.com
-              </td>
-              <td>
-                966271829
-              </td>
-              <td>
-                menu
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <Tabs aria-label="Tabs de estudiantes" variant="underline" onActiveTabChange={handleTabIndex}>
+          <Tabs.Item
+            title="Activos"
+            className="!rounded py-2"
+            active={tab === StudentTab.ACTIVE || tab === ''}
+          >
+            <table className="custom-table">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>
+                    Nombres
+                  </th>
+                  <th>
+                    Email
+                  </th>
+                  <th>
+                    N° teléfono
+                  </th>
+                  <th>
+                    Cursos
+                  </th>
+                  <th>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <TableLoading numCols={6} isLoading={isLoadingActive} />
+                {!isLoadingActive && activeStudents.map((student) => (
+                  <tr key={student.id}>
+                    <td>
+                      <img src="https://r2.yoelvalverde.dev/masthead.webp" alt="Image" className="w-8 h-8 rounded-full border border-primary-200 text-xs" />
+                    </td>
+                    <td>
+                      {student.firstName} {student.lastName}
+                    </td>
+                    <td>
+                      {student.email}
+                    </td>
+                    <td>
+                      {student.phone}
+                    </td>
+                    <td>
+                      {'3'}
+                    </td>
+                    <td>
+                      ...
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Tabs.Item>
+          <Tabs.Item
+            title="Registrados"
+            active={tab === StudentTab.REGISTERED}
+          >
+            <table className="custom-table">
+              <thead>
+                <tr>
+                  <th>
+                    Email
+                  </th>
+                  <th>
+                    N° teléfono
+                  </th>
+                  <th>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <TableLoading numCols={3} isLoading={isLoading} />
+                {!isLoading && preRegisteredStudents.map((student) => (
+                  <tr key={student.id}>
+                    <td>
+                      {student.email}
+                    </td>
+                    <td>
+                      {student.phone}
+                    </td>
+                    <td className="text-right">
+                      menu
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </Tabs.Item>
+        </Tabs>
       </section>
-
-      {show && (
-        <Suspense fallback={<ModalLoading />}>
-          <RegisterStudentModal isOpen={show} onClose={close} />
-        </Suspense>
-      )}
-    </div>
-  )
-}
-
-const ModalLoading = () => {
-  return (
-    <div className="fixed top-0 left-0 w-full h-full bg-transparent flex-center">
-      <div className="bg-white px-4 py-2 rounded-lg">Cargando modal</div>
     </div>
   )
 }
