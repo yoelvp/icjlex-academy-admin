b182053f9f76b2ef78b0c7e915c4e4badbc3a38d
yoelvp <yoelvp73@gmail.com>
2024-10-19 22:54:55 -0500

    fix: changs tyles

--- src/@common/constants/menu-variants.ts
+++ src/@common/constants/menu-variants.ts
@@ -1,9 +1,10 @@
 import { cva } from 'class-variance-authority'
 
-export const menuVariants = cva('absolute z-50 bg-white rounded-sm py-4', {
+export const menuVariants = cva('absolute z-50 bg-white rounded-sm', {
   variants: {
     size: {
-      xs: 'w-auto py-2 shadow--primary',
+      xs: 'w-auto shadow--primary',
+      sm: 'w-auto shadow--primary',
       md: 'w-40',
       lg: 'w-80'
     },
