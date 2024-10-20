0befc7a8dc312b09639637ecd50df049932f5874
yoelvp <yoelvp73@gmail.com>
2024-10-19 22:57:00 -0500

    fix: change stores

--- src/modules/students/store/use-students.store.ts
+++ src/modules/students/store/use-students.store.ts
@@ -5,13 +5,21 @@ import { create } from 'zustand'
 interface UseStudentStore {
   activeStudents: Student[]
   preRegisteredStudents: StudentPreRegistration[]
+  preRegistered: StudentPreRegistration | null
+  studentId: string | null
   setActiveStudents: (students: Student[]) => void
   setPreRegisteredStudents: (students: StudentPreRegistration[]) => void
+  setStudentId: (id: string | null) => void
+  setPreRegistered: (student: StudentPreRegistration | null) => void
 }
 
 export const useStudentsStore = create<UseStudentStore>()((set) => ({
   activeStudents: [],
   preRegisteredStudents: [],
+  studentId: null,
+  preRegistered: null,
   setActiveStudents: (newStudents: Student[]) => set({ activeStudents: newStudents }),
-  setPreRegisteredStudents: (newStudents: StudentPreRegistration[]) => set({ preRegisteredStudents: newStudents })
+  setPreRegisteredStudents: (newStudents: StudentPreRegistration[]) => set({ preRegisteredStudents: newStudents }),
+  setStudentId: (id: string | null) => set({ studentId: id }),
+  setPreRegistered: (student: StudentPreRegistration | null) => set({ preRegistered: student })
 }))
