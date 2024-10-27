16311fb0fc166365cf81866c12a90d6a20640a56
yoelvp <yoelvp73@gmail.com>
2024-10-27 15:49:10 -0500

    fix: implement get courses in client views

--- src/modules/[client]/home/components/specialized-courses.tsx
+++ src/modules/[client]/home/components/specialized-courses.tsx
@@ -1,10 +1,18 @@
-import type { FC } from 'react'
+import { useEffect, type FC } from 'react'
 
 import { Content } from '@/@common/components/content'
 import { SectionHeader } from '@/@common/components/section-header'
 import { CourseCard } from '@/@common/components/course-card'
+import { useCourses } from '../hooks/use-courses'
+import { Spinner } from 'flowbite-react'
 
 export const SpecializedCourses: FC = () => {
+  const { courses, isLoadingCourses, getAllCourses } = useCourses()
+
+  useEffect(() => {
+    getAllCourses()
+  }, [])
+
   return (
     <Content className="flex flex-col gap-y-8">
       <SectionHeader
@@ -16,9 +24,12 @@ export const SpecializedCourses: FC = () => {
       />
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
+        {isLoadingCourses && (
+          <Spinner />
+        )}
+        {!isLoadingCourses && courses?.map((course) => (
+          <CourseCard key={course.id} course={course} />
+        ))}
       </div>
     </Content>
   )
