1fa604897c574e7c1e46a2ab6ac04872247a892e
valdir169 <contrerasvaldir06@gmail.com>
2024-10-27 01:45:44 -0500

    feat: implement courseId store

--- src/modules/courses/store/course.store.ts
+++ src/modules/courses/store/course.store.ts
@@ -5,14 +5,19 @@ import { DEFAULT_PAGINATION } from '@/@common/constants/default-pagination'
 
 interface UseCourseData {
   courses: CourseResult[]
-  setCourses: (course: CourseResult[]) => void
   pagination: Pagination
+  courseId: string | null
+  setCourses: (course: CourseResult[]) => void
   setPagination: (pagination: Pagination) => void
+  setCourseId: (id: string | null) => void
 }
 
 export const UseCourseStore = create<UseCourseData>((set) => ({
   courses: [],
   pagination: DEFAULT_PAGINATION,
+  courseId: null,
   setCourses: (courses: CourseResult[]) => set({ courses }),
-  setPagination: (pagination: Pagination) => set((state) => ({ ...state, pagination }))
+  setPagination: (pagination: Pagination) =>
+    set((state) => ({ ...state, pagination })),
+  setCourseId: (id: string | null) => set({ courseId: id })
 }))
