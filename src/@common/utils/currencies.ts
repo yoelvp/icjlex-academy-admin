78ef37f70eb2d117540443c4e8d2cef3a96a2e92
yoelvp <yoelvp73@gmail.com>
2024-10-27 15:47:50 -0500

    feat: add format for currency

---
+++ src/@common/utils/currencies.ts
@@ -0,0 +1,8 @@
+export const formatCurrency = (amount: number | null) => {
+  return new Intl.NumberFormat('es-PE', {
+    style: 'currency',
+    currency: 'PEN',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(amount ?? 0)
+}
