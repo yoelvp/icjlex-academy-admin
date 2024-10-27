e420c4fb43f41e47130c5953c8b79970806da690
valdir169 <contrerasvaldir06@gmail.com>
2024-10-27 01:43:59 -0500

    fix: optmize schema of course

--- src/modules/courses/schemas/course.schema.tsx
+++ src/modules/courses/schemas/course.schema.tsx
@@ -1,48 +1,55 @@
-import { array, mixed, object, string } from 'yup'
+import { array, boolean, mixed, object, string } from 'yup'
 
 export const courseSchema = object().shape({
   name: string().required('El nombre del curso es obligatorio'),
-  objetive: string().required('El objetivo es obligatorio'),
   docentId: string().required('Seleccionar un docente es obligatorio'),
-  content: array()
+  features: array()
     .of(
       object().shape({
-        title: string().required('El título es obligatorio'),
-        details: array()
-          .of(
-            object().shape({
-              titleClass: string().required(
-                'El título de la clase es obligatorio'
-              ),
-              duration: string()
-                .matches(
-                  /^\d{2}:\d{2}:\d{2}$/,
-                  'La duración debe estar en el formato HH:MM:SS'
-                )
-                .required('La duración es obligatoria'),
-              videoUrl: string()
-                .url('La URL del video no es válida')
-                .required('La URL del video es obligatoria')
-            })
-          )
-          .required('Los detalles son obligatorios')
+        name: string()
+          .required('El campo name es obligatorio')
+          .min(2, 'El name debe tener al menos 2 caracteres'),
+        value: string()
+          .required('El campo value es obligatorio')
+          .min(2, 'El value debe tener al menos 2 caracteres')
       })
     )
-    .required('El contenido es obligatorio'),
+    .required('Debes agregar al menos un elemento a Características')
+    .min(1, 'Debe haber al menos un elemento en Características'),
+  isActive: boolean()
+    .required('El campo isActive es obligatorio')
+    .oneOf([true, false], 'El campo isActive solo puede ser true o false'),
+  objetive: string().required('El objetivo es obligatorio'),
   image: mixed()
+    .nullable() // Permitir null
     .required('La imagen es obligatoria.')
     .test('fileSize', 'La imagen es muy grande.', (value) => {
-      // Si `value` no está definido, devuelve un error
-      if (!value) return false
-      // Si `value` es un array, toma el primer elemento
-      const file = Array.isArray(value) ? value[0] : value
-
-      return file.size <= 2 * 1024 * 1024 // Limitar a 2MB
+      return value && value instanceof File && value.size <= 2 * 1024 * 1024
     })
     .test('fileType', 'El formato de archivo no es válido.', (value) => {
-      if (!value) return false
-      const file = Array.isArray(value) ? value[0] : value
-
-      return ['image/jpeg', 'image/png', 'image/gif'].includes(file.type)
+      return (
+        value &&
+        value instanceof File &&
+        ['image/jpeg', 'image/png', 'image/gif'].includes(value.type)
+      )
+    }),
+  price: string()
+    .matches(
+      /^\d+(\.\d{1,2})?$/,
+      'El precio debe ser un número con hasta dos decimales'
+    )
+    .optional(),
+  startDate: mixed()
+    .test(
+      'isValidDate',
+      'La fecha debe estar en formato ISO o vacía',
+      (value) =>
+        value === '' ||
+        (typeof value === 'string' && !isNaN(Date.parse(value))) ||
+        value instanceof Date
+    )
+    .transform((value) => {
+      // Convierte una cadena ISO en una fecha Date
+      return typeof value === 'string' && value !== '' ? new Date(value) : value
     })
 })
