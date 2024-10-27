9e1c66b4987ff44fc0709cd4c0d44380ec8ad645
valdir169 <contrerasvaldir06@gmail.com>
2024-10-27 01:42:11 -0500

    feat: create hook ui for tabs

---
+++ src/modules/courses/hooks/use-courses-ui.ts
@@ -0,0 +1,17 @@
+import { useSearchParams } from 'react-router-dom'
+import { TAB_INDEX } from '../utils/course-tab-index'
+
+export const useCourseUI = () => {
+  const [queryParams, setQueryParams] = useSearchParams()
+  const tab = queryParams.get('tab')
+
+  const handleTabIndex = (index: number) => {
+    setQueryParams(TAB_INDEX[index])
+  }
+
+  return {
+    queryParams,
+    tab,
+    handleTabIndex
+  }
+}
