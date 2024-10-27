12e82c52a42509c085c694a6b4eea2d2c54b336c
valdir169 <contrerasvaldir06@gmail.com>
2024-10-27 01:46:14 -0500

    feat: add type contentCourse

--- src/modules/courses/types/Course.tsx
+++ src/modules/courses/types/Course.tsx
@@ -1,12 +1,18 @@
 import { InferType } from 'yup'
 import { courseSchema } from '../schemas/course.schema'
+import { resourceCourseSchema } from '../schemas/resources-from-course.schema'
 
 export type Course = InferType<typeof courseSchema>
+
+export type ContentCourse = InferType<typeof resourceCourseSchema>
 
 export interface CourseResult {
   id?: string
   name?: string
   objetive?: string
   imageUrl?: null | string
+  price: number
+  features: string
+  startDate: string
   isActive?: boolean
 }
