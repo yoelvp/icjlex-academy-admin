b9d052372284e4ac4b63084b9abc8e4a3212f1e3
yoelvp <yoelvp73@gmail.com>
2024-10-27 15:48:16 -0500

    fix: add new services for get in user view

--- src/_services/courses.service.ts
+++ src/_services/courses.service.ts
@@ -1,6 +1,17 @@
 import { IdAndNameFields } from '@/@common/types/IdAndName'
+import { Course } from '@/_models/Course.model'
 import { axios } from '@/lib'
 
 export const getAllCoursesOnlyNameService = () => {
   return axios.get<IdAndNameFields[]>('/courses/only-name')
+}
+
+/** Course for final user/client */
+export const getCoursesService = () => {
+  return axios.get<Course[]>('/courses/client', {
+    params: {
+      page: 1,
+      size: 3
+    }
+  })
 }
