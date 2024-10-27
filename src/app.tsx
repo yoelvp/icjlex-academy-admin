a1c04c4ee16e455a2e537fdce87e81ccaf40fe07
yoelvp <yoelvp73@gmail.com>
2024-10-27 15:50:39 -0500

    fix: add container for confirm modal

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,14 @@ import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import routes from '@/routes'
 import { AxiosInterceptor } from './lib'
+import { ContainerConfirmModal } from '@/@common/components'
 
 AxiosInterceptor()
 
 const App = () => {
   return (
     <>
+      <ContainerConfirmModal />
       <Toaster
         richColors
         position="top-right"
