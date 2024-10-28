05ee0af5dc401ae6c60ca50db08a1ec7377dcabc
valdir169 <contrerasvaldir06@gmail.com>
2024-10-27 22:38:52 -0500

    fix: change param slug to id in courses

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -56,7 +56,7 @@ const routes = createBrowserRouter([
         element: <CoursesPage />
       },
       {
-        path: '/courses/:slug',
+        path: '/courses/:id',
         element: <CourseDetailsPage />
       }
     ]
