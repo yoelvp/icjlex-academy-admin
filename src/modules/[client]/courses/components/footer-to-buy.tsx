c8b81eaeff35b1936d36e2b5239371146944ec35
valdir169 <contrerasvaldir06@gmail.com>
2024-10-27 22:51:43 -0500

    feat: render price in footer course card

--- src/modules/[client]/courses/components/footer-to-buy.tsx
+++ src/modules/[client]/courses/components/footer-to-buy.tsx
@@ -1,10 +1,12 @@
-import { FC } from 'react'
 import { PriceAndPurchaseButton } from './price-and-purchase-button'
+import { useFormattedPrice } from '../utils/format-price'
 
-export const FooterToBuy: FC = () => {
+export const FooterToBuy = ({ price }) => {
+  const formattedPrice = useFormattedPrice(price)
+
   return (
     <div className="w-full bg-primary-500 fixed left-0 bottom-0 z-50 shadow shadow-white lg:hidden">
-      <PriceAndPurchaseButton />
+      <PriceAndPurchaseButton price={formattedPrice} />
     </div>
   )
 }
