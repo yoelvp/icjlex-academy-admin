218d22a59509ae161fc0b4329f06bd8b891cd5ff
valdir169 <contrerasvaldir06@gmail.com>
2024-10-27 22:48:28 -0500

    feat: implement search by course name

--- src/modules/[client]/courses/components/search-course.tsx
+++ src/modules/[client]/courses/components/search-course.tsx
@@ -1,25 +1,28 @@
 import { Input } from '@/@common/components/form/input'
 import { IconSearch } from '@/assets/icons'
 
-export const SearchCourse = () => {
+export const SearchCourse = ({ coursesCounter, loading, setSearchTerm }) => {
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value) // Actualizar el estado del término de búsqueda
+  }
+
   return (
-    <div className="flex flex-col gap-4 md:flex-between md:flex-row  lg:col-span-3">
+    <div className="flex flex-col gap-4 md:flex-between md:flex-row lg:col-span-4">
       <span className="text-primary-500 font-semibold text-xl">
-        10 cursos encontrados
+        {loading ? 'Cargando...' : `${coursesCounter ?? 0} cursos encontrados`}
       </span>
       <div className="flex-between gap-4 px-4 border border-primary-500 rounded-lg">
         <IconSearch size={24} />
         <Input
           type="search"
-          name=""
-          id=""
-          placeholder="Busca cualquier cosa"
+          placeholder="Busca cualquier curso"
           className="border-none outline-none focus:outline-none"
           style={{
             boxShadow: 'none',
             WebkitAppearance: 'none',
             MozAppearance: 'none'
           }}
+          onChange={handleSearchChange}
         />
       </div>
     </div>
