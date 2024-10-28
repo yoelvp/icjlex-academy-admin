7d82250e63e9c2d4b252370aa5e16cb3816490a0
valdir169 <contrerasvaldir06@gmail.com>
2024-10-27 22:46:28 -0500

    feat: render detail of each course

--- src/modules/[client]/courses/views/(slug).tsx
+++ src/modules/[client]/courses/views/(slug).tsx
@@ -7,25 +7,57 @@ import { ContentDescription } from '../components/content-description'
 import { CoursesValorations } from '../components/courses-valorations'
 import { MoreCoursesByTeacher } from '../components/more-courses-by-teacher'
 import { FooterToBuy } from '../components/footer-to-buy'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { CourseResult } from '@/modules/courses/types/Course'
+import { getCourseByIdService } from '@/modules/courses/service/get-course-by-id.service'
+import { ContentFull } from '@/@common/components/content-full'
 
 const CourseDetailsPage = () => {
+  const { id } = useParams()
+  const [course, setCourse] = useState<CourseResult | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchCourse = async () => {
+      try {
+        const { data } = await getCourseByIdService(id)
+        setCourse(data)
+      } catch (error) {
+        console.log('Error al carga el detalle del curso', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchCourse()
+  }, [id])
+
   return (
     <>
-      <HeroPage />
+      {loading ? (
+        <ContentFull containerClassName="min-h-[60vh] py-32">
+          <p className="text-primary-300">Cargando detalles del curso...</p>
+        </ContentFull>
+      ) : (
+        <>
+          <HeroPage course={course} />
 
-      <Content className="flex flex-col items-start mt-16 mb-24 gap-y-16 md:mt-32 md:mb-40 xl:gap-x-32 lg:gap-x-16 lg:flex-row-reverse">
-        <DetailsCourseByPurchased />
+          <Content className="flex flex-col items-start mt-16 mb-24 gap-y-16 md:mt-32 md:mb-40 xl:gap-x-32 lg:gap-x-16 lg:flex-row-reverse">
+            <DetailsCourseByPurchased course={course} />
 
-        <div className="w-full flex flex-col gap-y-16">
-          <WillLearn />
-          <ContentVariousVideos />
-          <ContentDescription />
-          <CoursesValorations />
-          <MoreCoursesByTeacher />
-        </div>
-      </Content>
+            <div className="w-full flex flex-col gap-y-16">
+              <WillLearn />
+              <ContentVariousVideos />
+              <ContentDescription />
+              <CoursesValorations />
+              <MoreCoursesByTeacher />
+            </div>
+          </Content>
 
-      <FooterToBuy />
+          <FooterToBuy price={course?.price} />
+        </>
+      )}
     </>
   )
 }
