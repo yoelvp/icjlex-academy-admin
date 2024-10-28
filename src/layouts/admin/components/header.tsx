a35f740ae994a2b1904a1c6c890575c66820ed1b
yoelvp <yoelvp73@gmail.com>
2024-10-27 20:04:43 -0500

    fix: validate when the user is authenticated

--- src/layouts/admin/components/header.tsx
+++ src/layouts/admin/components/header.tsx
@@ -1,9 +1,14 @@
 import { useSidebar } from '@/store/use-sidebar.store'
-import { IconChevronDown, IconMenuLeft, IconMenuRight } from '@/assets/icons'
+import { IconMenuLeft, IconMenuRight } from '@/assets/icons'
+import { Menu } from '@/@common/components'
+import { UserMenu } from '@/layouts/client/components/header/user-menu'
+import { getUserInitials } from '@/@common/utils/get-initials'
+import { useUserStore } from '@/@auth/store/use-user.store'
 
 export const Header  = () => {
   const toggle = useSidebar((state) => state.toggle)
   const show = useSidebar((state) => state.show)
+  const user = useUserStore((state) => state.user)
 
   return (
     <header className="w-full bg-primary-500 h-16 text-white px-4 flex items-center justify-between">
@@ -15,16 +20,15 @@ export const Header  = () => {
         {show ? <IconMenuLeft size="20" /> : <IconMenuRight size="20" />}
       </button>
 
-      <button className="flex-center gap-x-4">
-        <div className="flex-center text-sm w-8 h-8 rounded-full bg-primary-100 text-primary-900">
-          YV
+      {user ? (
+        <Menu activator={getUserInitials(user)} variant="white">
+          <UserMenu />
+        </Menu>
+      ) : (
+        <div>
+          No se ecnuentra el usuario
         </div>
-        <div className="flex items-start flex-col">
-          <strong>Yoel Valverde</strong>
-          <span className="text-xs font-medium">Administrador</span>
-        </div>
-        <IconChevronDown />
-      </button>
+      )}
     </header>
   )
 }
