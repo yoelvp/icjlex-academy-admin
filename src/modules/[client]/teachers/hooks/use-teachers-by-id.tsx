a067b53fd11773eefac5a666537315d4009771ef
valdir169 <contrerasvaldir06@gmail.com>
2024-10-29 00:35:53 -0500

    feat: implement hook use teacher by id

---
+++ src/modules/[client]/teachers/hooks/use-teachers-by-id.tsx
@@ -0,0 +1,33 @@
+import { useLoading } from '@/@common/hooks/use-loading'
+import getError from '@/@common/utils/get-errors'
+import { getTeacherByIdService } from '@/modules/teachers/service/docents.service'
+import { DocentResult } from '@/modules/teachers/types/Docent'
+import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
+
+export const useTeachersById = (id: string | undefined) => {
+  const [teachers, setTeachers] = useState<DocentResult | null>(null)
+  const { isLoading, loaded, loading } = useLoading()
+
+  useEffect(() => {
+    if (!id) return
+
+    const fetchTeachers = async () => {
+      try {
+        loading()
+        const { data } = await getTeacherByIdService(id)
+        setTeachers(data)
+      } catch (error) {
+        loaded()
+        const { message } = getError(error)
+        toast.error(message)
+      } finally {
+        loaded()
+      }
+    }
+
+    fetchTeachers()
+  }, [id])
+
+  return { teachers, isLoading }
+}
