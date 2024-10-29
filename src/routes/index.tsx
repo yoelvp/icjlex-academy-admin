a8635e977440e462badf227a02ae13f7033d9bea
valdir169 <contrerasvaldir06@gmail.com>
2024-10-29 00:22:37 -0500

    fix: change params slug by id

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -48,7 +48,7 @@ const routes = createBrowserRouter([
         element: <TeachersPage />
       },
       {
-        path: '/teachers/:slug',
+        path: '/teachers/:id',
         element: <TeacherDetailsPage />
       },
       {
