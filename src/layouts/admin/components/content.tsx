4ef2c9cfbde584fe92c7f0cf5f709449e7499e16
yoelvp <yoelvp73@gmail.com>
2024-10-30 12:50:54 -0500

    fix: change styles and clasess

--- src/layouts/admin/components/content.tsx
+++ src/layouts/admin/components/content.tsx
@@ -8,7 +8,7 @@ const Content: FC<Props> = ({
   children
 }) => {
   return (
-    <div className="px-6 py-8 bg-primary-50/60">
+    <div className="h-full px-6 py-8 bg-primary-50/60">
       {children}
     </div>
   )
