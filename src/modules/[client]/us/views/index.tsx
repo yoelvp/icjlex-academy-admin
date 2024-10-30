acaec22ea6997a82139f116ce26b57a39ea5296f
yoelvp <yoelvp73@gmail.com>
2024-10-30 13:15:36 -0500

    fix: remove unused variables

--- src/modules/[client]/us/views/index.tsx
+++ src/modules/[client]/us/views/index.tsx
@@ -13,7 +13,7 @@ import Collaborators from '../components/collaborators'
 import { useTeachers } from '../../teachers/hooks/use-teachers'
 
 const UsPage = () => {
-  const { isLoading, teachers } = useTeachers(1, 6)
+  const { teachers } = useTeachers(1, 6)
 
   return (
     <>
