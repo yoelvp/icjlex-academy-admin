123e701e4709815849c3a51db6836588991d398f
yoelvp <yoelvp73@gmail.com>
2024-10-31 11:46:46 -0500

    fix: add new types

--- src/modules/teachers/types/Docent.ts
+++ src/modules/teachers/types/Docent.ts
@@ -2,6 +2,16 @@ import { InferType } from 'yup'
 import { docentSchema } from '../schemas/docent.schema'
 
 export type Docent = InferType<typeof docentSchema>
+export type DocentSchema = InferType<typeof docentSchema>
+
+export interface DocentFields extends InferType<typeof docentSchema> {
+  id?: string
+}
+
+export interface TeacherData extends Omit<InferType<typeof docentSchema>, 'specialties'> {
+  id?: string
+  specialties: (string | undefined)[]
+}
 
 export type DocentResult = {
   id: string
