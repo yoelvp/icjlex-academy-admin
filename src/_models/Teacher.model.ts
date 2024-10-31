739dfc11a6fb8229d51acd4a1aee0de7a226efde
yoelvp <yoelvp73@gmail.com>
2024-10-31 11:45:02 -0500

    feat: add new types

--- src/_models/Teacher.model.ts
+++ src/_models/Teacher.model.ts
@@ -1,6 +1,24 @@
 export interface TeacherBasicData {
   firstName: string
   lastName: string
-  imageUrl: string
   profession: string
+  imageUrl: string | null
+}
+
+export interface Teacher {
+  id: string
+  firstName: string
+  lastName: string
+  profession: string
+  about: string
+  imageUrl: string | null
+  specialties: string[]
+  socialMedia: {
+    linkedin: string | null
+    youtube: string | null
+    facebook: string | null
+    x: string | null
+    instagram: string | null
+    whatsapp: string | null
+  }
 }
