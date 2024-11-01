478644c4dcd43ccf330455e96bff71a15a58a22a
yoelvp <yoelvp73@gmail.com>
2024-11-01 12:09:30 -0500

    fix: change color loader

--- src/@common/components/skeleton/index.tsx
+++ src/@common/components/skeleton/index.tsx
@@ -1,18 +1,16 @@
 import classNames from 'classnames'
 
 interface Props {
-  color?: 'primary' | 'gray'
   className?: string
 }
 
 export const Skeleton = ({
-  color = 'gray',
   className
 }: Props) => {
   return (
     <div
       className={classNames(
-        `bg-gradient-to-r from-${color}-100 via-${color}-300 to-${color}-100 rounded animate-pulse`,
+        'bg-gradient-to-r from-gray-100 via-gray-300 to-gray-100 rounded animate-pulse',
         className
       )}
     />
