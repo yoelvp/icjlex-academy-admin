b3da15d3515942071483e818aaff5d472c11c4d7
yoelvp <yoelvp73@gmail.com>
2024-11-01 18:56:54 -0500

    fix: add new types for manage form

---
+++ src/modules/courses/types/CourseFormFields.ts
@@ -0,0 +1,16 @@
+import { InferType } from 'yup'
+import { courseSchema } from '../schemas/course.schema'
+
+export type CourseFields = InferType<typeof courseSchema>
+
+export interface CourseFormData extends Omit<
+  InferType<typeof courseSchema>,
+  'includes'
+  | 'youWillLearn'
+  | 'image'
+> {
+  includes: string[]
+  youWillLearn: string[]
+  image: File | null
+  publicationDate?: Date | string
+}
