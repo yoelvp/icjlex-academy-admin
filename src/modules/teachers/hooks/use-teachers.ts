7941f1c70a96593124c68ed01bffd838b158bea7
yoelvp <yoelvp73@gmail.com>
2024-11-01 12:11:04 -0500

    feat: add hook for update image

---
+++ src/modules/teachers/hooks/use-teachers.ts
@@ -0,0 +1,36 @@
+import { toast } from 'sonner'
+import { HttpStatusCode } from 'axios'
+import { useLoading } from '@/@common/hooks/use-loading'
+import getError from '@/@common/utils/get-errors'
+import { updateImageTeacherService } from '@/_services/teachers.service'
+import { UpdateTeacherImage } from '../types/TeacherFormFields'
+import { useTeacherStore } from '../store/teachers.store'
+
+export const useTeachers = () => {
+  const { isLoading, loading, loaded } = useLoading()
+  const setTeachers = useTeacherStore((state) => state.setTeachers)
+  const teachers = useTeacherStore((state) => state.teachers)
+
+  const updateImage = async (teacherId: string, bodyData: UpdateTeacherImage) => {
+    try {
+      loading()
+      const { data, status } = await updateImageTeacherService(teacherId, bodyData)
+
+      if (status === HttpStatusCode.Ok) {
+        const updatedTeachers = teachers.map((teacher) => teacher.id === data.teacherId ? { ...teacher, imageUrl: data.imageUrl ?? '' } : teacher)
+        setTeachers(updatedTeachers)
+      }
+    } catch (error) {
+      loaded()
+      const { message } = getError(error)
+      toast.error(message)
+    } finally {
+      loaded()
+    }
+  }
+
+  return {
+    isLoading,
+    updateImage
+  }
+}
