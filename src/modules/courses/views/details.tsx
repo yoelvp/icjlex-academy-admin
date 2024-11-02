4dd837455926cbc3639fcdbbfcafae7515a4eb03
yoelvp <yoelvp73@gmail.com>
2024-11-02 18:59:45 -0500

    feat: add course details

--- src/modules/courses/views/details.tsx
+++ src/modules/courses/views/details.tsx
@@ -1,31 +1,41 @@
-import { lazy, Suspense } from 'react'
+import type { IdParams } from '@/@common/types'
+
+import { lazy, Suspense, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import classNames from 'classnames'
+import { List, Tabs } from 'flowbite-react'
+import Button from '@/@common/components/button'
+import { useShow } from '@/@common/hooks/use-show'
+import { TableLoading } from '@/@common/components/table-loading'
+import { LoadingModal, Menu, RenderHTML } from '@/@common/components'
+import { useCoursesInformation } from '../hooks/use-get-courses-information'
 import {
   IconAdd,
   IconArrowRoundBack,
+  IconCheckmark,
   IconDelete,
   IconEdit,
   IconEye,
   IconList
 } from '@/assets/icons'
-import Button from '@/@common/components/button'
-import { useShow } from '@/@common/hooks/use-show'
-import { TableLoading } from '@/@common/components/table-loading'
-import { LoadingModal, Menu } from '@/@common/components'
-import { Link } from 'react-router-dom'
-import classNames from 'classnames'
-import { List, Tabs } from 'flowbite-react'
-import { useCoursesInformation } from '../hooks/use-get-courses-information'
+import { formatCurrency } from '@/@common/utils/currencies'
+import Form from '@/@common/components/form'
 
-const ResourcesFromCourse = lazy(
-  () => import('../components/resources-from-course')
-)
+const ResourcesFromCourse = lazy(() => import('../components/resources-from-course'))
 
 const CoursesPage = () => {
+  const [isEditingTitle, setIsEditingTitle] = useState(false)
   const { show, open, close } = useShow()
   const { courses, isLoading } = useCoursesInformation(1, 999)
+  const params = useParams<IdParams>()
+  console.log(params.id)
+
+  const handleEditTitle = () => {
+    setIsEditingTitle(true)
+  }
 
   return (
-    <div className="flex-col gap-y-8 grid grid-rows-[auto_1fr]">
+    <div className="gap-y-4 grid grid-rows-[auto_1fr]">
       <header className="section-panel header-height flex-between">
         <div className="flex-start gap-x-2">
           <Link
@@ -45,19 +55,145 @@ const CoursesPage = () => {
           Agregar
         </Button>
       </header>
+
       <section className="section-panel p-4">
         <Tabs
           aria-label="Tabs de detalles"
           variant="underline"
-          /* onActiveTabChange={handleTabIndex} */
         >
-          {/* <Tabs.Item
-            title="Información principal"
-          >
-            Buenas noches
-          </Tabs.Item> */}
           <Tabs.Item
             title="Información principal"
+          >
+            <div className="flex flex-col gap-y-16">
+              <div className="grid grid-cols-2 gap-x-8">
+                <article>
+                  <div className="flex items-center gap-x-4 py-4">
+                    {!isEditingTitle ? (
+                      <>
+                        <h3 className="text-primary-700 text-xl font-bold">
+                          Nombre del curso
+                        </h3>
+                        <button
+                          type="button"
+                          onClick={handleEditTitle}
+                          className="flex-center w-8 h-8 rounded-xs transition-colors hover:bg-zinc-100"
+                        >
+                          <IconEdit size="16" />
+                        </button>
+                      </>
+                    ) : (
+                      <>
+                        <Form.Input
+                          type="text"
+                          size="md"
+                          value="Nombre del curso"
+                          placeholder="Ingresa el nuevo título del curso"
+                        />
+                        <button
+                          className="w-10 h-10 flex-center bg-primary-500 hover:bg-primary-600 text-white rounded-sm"
+                          onClick={() => setIsEditingTitle(false)}
+                        >
+                          <IconCheckmark size="24" />
+                        </button>
+                      </>
+                    )}
+                  </div>
+                  <hr className="line-sep-h" />
+                  <div className="mt-4">
+                    <h4 className="text-primary-700 text-lg font-bold">
+                      Objetivo
+                    </h4>
+                    <p>
+                      Lorem
+                    </p>
+                  </div>
+                </article>
+                <div className="space-y-4">
+                  <img
+                    src={'/image-placeholder.png'}
+                    alt="Thumbnail of course"
+                    className="w-full h-64 object-cover object-center rounded-xs"
+                  />
+                  <div className="grid grid-cols-2 gap-x-4">
+                    <div className="flex flex-col gap-y-1">
+                      <label className="text-zinc-600 text-sm">
+                        Fecha de publicación
+                      </label>
+                      <strong className="text-primary-700 text-lg">
+                        {'14 de junio de 2024'}
+                      </strong>
+                    </div>
+                    <div className="flex flex-col gap-y-1">
+                      <label className="text-zinc-600 text-sm">
+                        Precio
+                      </label>
+                      <strong className="text-primary-700 text-lg">
+                        {formatCurrency(20)}
+                      </strong>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="grid grid-cols-2 gap-x-8">
+                <div className="space-y-2">
+                  <h4 className="text-primary-700 text-lg font-bold">
+                    Lo que incluye el curso
+                  </h4>
+                  <ul className="grid grid-cols-2 list-disc list-inside">
+                    <li>Hola 01</li>
+                    <li>Hola 02</li>
+                    <li>Hola 03</li>
+                    <li>Hola 04</li>
+                    <li>Hola 05</li>
+                    <li>Hola 06</li>
+                    <li>Hola 07</li>
+                  </ul>
+                </div>
+                <div className="space-y-2">
+                  <h4 className="text-primary-700 text-lg font-bold">
+                    Lo que aprenderá
+                  </h4>
+                  <ul>
+                    <li>Hola</li>
+                    <li>Hola 02</li>
+                    <li>Hola 03</li>
+                    <li>Hola 04</li>
+                  </ul>
+                </div>
+              </div>
+
+              <div>
+                <h4 className="text-primary-700 text-lg font-bold mb-4">
+                  Descripción del curso
+                </h4>
+                <div>
+                  <RenderHTML content="<p>Un buen curso</p>" />
+                </div>
+              </div>
+
+              <div>
+                <h4 className="text-primary-700 text-lg font-bold mb-2">
+                  Docentes
+                </h4>
+                <div className="flex gap-x-4 items-center justify-start bg-primary-50/50 px-4 py-8 rounded-sm">
+                  <div className="w-12 h-12 rounded-full bg-zinc-200 flex-center text-sm font-bold text-zinc-600">
+                    YV
+                  </div>
+                  <div className="flex flex-col">
+                    <strong>
+                      {'Yoel Valverde'}
+                    </strong>
+                    <span>
+                      {'Frontend developer'}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Tabs.Item>
+          <Tabs.Item
+            title="Detalles"
           >
             <table className="custom-table mb-6">
               <thead>
