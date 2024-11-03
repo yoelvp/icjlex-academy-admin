755a699b8851b209d5b56c6ddfdbee7751bcc970
valdir169 <contrerasvaldir06@gmail.com>
2024-11-03 01:06:27 -0500

    fix: use hook component

--- src/modules/[client]/courses/views/index.tsx
+++ src/modules/[client]/courses/views/index.tsx
@@ -2,45 +2,33 @@ import { Content } from '@/@common/components/content'
 import { ListOfCourses } from '../components/list-of-courses'
 import { SearchCourse } from '../components/search-course'
 import { useEffect, useState } from 'react'
-import { getAllCoursesService } from '@/modules/courses/service/course.service'
-import { CourseResult } from '@/modules/courses/types/Course'
+import { Spinner } from 'flowbite-react'
+import { useCourses } from '../../hooks/use-courses'
 
 const CoursesPage = () => {
-  const [courses, setCourses] = useState<CourseResult[]>([])
-  const [coursesCounter, setCoursesCounter] = useState<number | null>(null)
-  const [loading, setLoading] = useState(true)
   const [searchTerm, setSearchTerm] = useState('')
+  const { courses, getAllCourses, isLoadingCourses } = useCourses()
 
   useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const { data } = await getAllCoursesService(1, 9)
-        setCourses(data.results)
-        setCoursesCounter(data.count)
-      } catch (error) {
-        console.log('Error fetching course', error)
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchCourses()
+    getAllCourses()
   }, [])
 
-  // Filter courses based on search term
-  const filteredCourses = courses.filter((course) => {
-    return course.name?.toLowerCase().includes(searchTerm.toLowerCase())
-  })
+  const filteredCourses = courses
+    ? courses.filter((course) =>
+      course.name?.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    : []
 
   return (
     <Content className="py-32 flex flex-col lg:grid grid-cols-1 gap-8 lg:grid-cols-4">
       <SearchCourse
-        coursesCounter={coursesCounter}
-        loading={loading}
+        coursesCounter={courses?.length}
+        loading={isLoadingCourses}
         setSearchTerm={setSearchTerm}
       />
-      {loading ? (
+      {isLoadingCourses ? (
         <div className="h-screen">
-          <p className="text-primary-300">Cargando cursos...</p>
+          <Spinner />
         </div>
       ) : filteredCourses.length === 0 ? (
         <div className="flex-center text-primary-300 w-full col-span-4 min-h-[20vh]">
