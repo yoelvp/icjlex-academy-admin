9ab8be3e75a6b38b62a698ed7f6e179bc6ae39c8
valdir169 <contrerasvaldir06@gmail.com>
2024-11-03 01:11:16 -0500

    feat: pass as prop name footer to buy

--- src/modules/[client]/courses/components/footer-to-buy.tsx
+++ src/modules/[client]/courses/components/footer-to-buy.tsx
@@ -1,12 +1,12 @@
 import { PriceAndPurchaseButton } from './price-and-purchase-button'
 import { useFormattedPrice } from '../utils/format-price'
 
-export const FooterToBuy = ({ price }) => {
+export const FooterToBuy = ({ price,name }) => {
   const formattedPrice = useFormattedPrice(price)
 
   return (
     <div className="w-full bg-primary-500 fixed left-0 bottom-0 z-50 shadow shadow-white lg:hidden">
-      <PriceAndPurchaseButton price={formattedPrice} />
+      <PriceAndPurchaseButton price={formattedPrice} name={name} />
     </div>
   )
 }
