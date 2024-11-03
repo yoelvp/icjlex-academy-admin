7b2bfd9549602585c8e85b7272b1dcc9f85fc296
yoelvp <yoelvp73@gmail.com>
2024-11-03 16:55:42 -0500

    fix: change types and add new service for get contents

--- src/_services/courses.service.ts
+++ src/_services/courses.service.ts
@@ -1,25 +1,29 @@
 import type { PublishedCourse, UpcomingCourse } from '@/modules/courses/types/Course'
 import type { IdAndNameFields } from '@/@common/types/IdAndName'
-import type { ResponseData } from '@/@common/types/ResponseData'
-import type { Course } from '@/_models/Course.model'
+import type { ResWithPagination } from '@/@common/types/ResponseData'
+import type { Course, CourseContents } from '@/_models/Course.model'
 
 import { axios } from '@/lib'
 
 export const getAllPublishedCoursesService = () => {
-  return axios.get<ResponseData<PublishedCourse>>('/courses')
+  return axios.get<ResWithPagination<PublishedCourse>>('/courses')
 }
 
 export const getAllUpcomingCoursesService = () => {
-  return axios.get<ResponseData<UpcomingCourse>>('/courses/soon')
+  return axios.get<ResWithPagination<UpcomingCourse>>('/courses/soon')
 }
 
 export const getAllCoursesOnlyNameService = () => {
   return axios.get<IdAndNameFields[]>('/courses/only-name')
 }
 
+export const getAllCoursesContentsService = (courseId: string) => {
+  return axios.get<ResWithPagination<CourseContents>>(`/courses/${courseId}/contents`)
+}
+
 /** Course for final user/client */
 export const getCoursesService = () => {
-  return axios.get<ResponseData<Course>>('/courses/details', {
+  return axios.get<ResWithPagination<Course>>('/courses/details', {
     params: {
       page: 1,
       size: 99
