aaee62fbf5d8214d99b0dc25da9decb2485c3027
valdir169 <contrerasvaldir06@gmail.com>
2024-11-03 01:03:46 -0500

    feat: render data of upcoming courses

--- src/modules/[client]/home/components/upcoming-courses.tsx
+++ src/modules/[client]/home/components/upcoming-courses.tsx
@@ -1,10 +1,18 @@
-import type { FC } from 'react'
+import { useEffect, type FC } from 'react'
 
 import { Content } from '@/@common/components/content'
 import { SectionHeader } from '@/@common/components/section-header'
 import { CourseCard } from '@/@common/components/course-card'
+import { useCourses } from '../hooks/use-courses'
+import { Spinner } from 'flowbite-react'
 
 export const UpcomingCourses: FC = () => {
+  const { courses, isLoadingCourses, getAllCourses } = useCourses()
+  const upComingCourses = courses?.slice(6, 9)
+  useEffect(() => {
+    getAllCourses()
+  }, [])
+
   return (
     <Content className="flex flex-col gap-y-8">
       <SectionHeader
@@ -12,13 +20,15 @@ export const UpcomingCourses: FC = () => {
         description="No te pierdas la oportunidad de formarte con expertos del sector. Inscríbete en nuestros próximos cursos y lleva tu carrera al siguiente nivel."
         withButton
         buttonLabel="Ver todos los próximos cursos"
-        buttonHref="/course"
+        buttonHref="/courses?t=s"
       />
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
-        <CourseCard />
-        <CourseCard />
-        <CourseCard />
+        {isLoadingCourses && <Spinner />}
+        {!isLoadingCourses &&
+          upComingCourses?.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
       </div>
     </Content>
   )
