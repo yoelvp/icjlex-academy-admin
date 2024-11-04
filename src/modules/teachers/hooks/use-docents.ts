b2f8d772072a490398fde4d8328c7ba3c3c6f36d
yoelvp <yoelvp73@gmail.com>
2024-11-04 01:34:32 -0500

    fix: implementar pagination

--- src/modules/teachers/hooks/use-docents.ts
+++ src/modules/teachers/hooks/use-docents.ts
@@ -1,15 +1,19 @@
 import { useEffect } from 'react'
 import { toast } from 'sonner'
+import { HttpStatusCode } from 'axios'
 import { useTeacherStore } from '../store/teachers.store'
-import getError from '@/@common/utils/get-errors'
 import { useLoading } from '@/@common/hooks/use-loading'
 import { getAllTeachersService } from '@/_services/teachers.service'
+import { usePagination } from '@/@common/hooks/use-pagination'
+import { responseMapper } from '@/@common/utils/response-mapper'
+import getError from '@/@common/utils/get-errors'
 
 export const useDocents = (page: number, size: number) => {
   const { isLoading, loading, loaded } = useLoading()
   const setTeachers = useTeacherStore((state) => state.setTeachers)
   const setPagination = useTeacherStore((state) => state.setPagination)
   const pagination = useTeacherStore((state) => state.pagination)
+  const paging = usePagination(pagination)
 
   useEffect(() => {
     getAllTeachers()
@@ -18,14 +22,14 @@ export const useDocents = (page: number, size: number) => {
   const getAllTeachers = async () => {
     loading()
     try {
-      const { data } = await getAllTeachersService(page, size)
-      setTeachers(data.results)
-      setPagination({
-        count: data.count,
-        totalPages: data.totalPages,
-        currentPage: data.currentPage,
-        size: data.size
-      })
+      const { data: resData, status } = await getAllTeachersService(page, size)
+
+      if (status === HttpStatusCode.Ok) {
+        const data = responseMapper(resData)
+        setTeachers(data.results)
+
+        setPagination(data)
+      }
     } catch (error) {
       loaded()
       const { message } = getError(error)
@@ -37,6 +41,6 @@ export const useDocents = (page: number, size: number) => {
 
   return {
     isLoading,
-    pagination
+    pagination: paging
   }
 }
