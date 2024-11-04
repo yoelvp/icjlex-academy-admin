ea24fd37f158212c674d240342d503d4fc5d08dc
yoelvp <yoelvp73@gmail.com>
2024-11-04 01:33:38 -0500

    feat: get all course by client id

---
+++ src/modules/[client]/teachers/hooks/use-get-course-by-teacher-id.ts
@@ -0,0 +1,48 @@
+import type { Course } from '@/_models/Course.model'
+
+import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
+import { HttpStatus } from '@/_utils/http-status.enum'
+import { getCoursesService } from '@/_services/courses.service'
+import { useLoading } from '@/@common/hooks/use-loading'
+import { usePagination } from '@/@common/hooks/use-pagination'
+import { Pagination } from '@/@common/types/Pagination'
+import { DEFAULT_PAGINATION } from '@/@common/constants/default-pagination'
+import { responseMapper } from '@/@common/utils/response-mapper'
+import getError from '@/@common/utils/get-errors'
+
+export const useGetCoursesByTeacherId = () => {
+  const [courses, setCourses] = useState<Course[] | null>(null)
+  const [paginationState, setPaginationState] = useState<Pagination>(DEFAULT_PAGINATION)
+  const pagination = usePagination(paginationState)
+  const { isLoading, loading, loaded } = useLoading()
+
+  useEffect(() => {
+    getAllCourses()
+  }, [pagination.page, pagination.size])
+
+  const getAllCourses = async () => {
+    try {
+      loading()
+      const { data: resData, status } = await getCoursesService()
+
+      if (status === HttpStatus.OK) {
+        const data = responseMapper(resData)
+        setCourses(data.results)
+        setPaginationState(data)
+      }
+    } catch (error) {
+      loaded()
+      const { message } = getError(error)
+      toast.error(message)
+    } finally {
+      loaded()
+    }
+  }
+
+  return {
+    isLoading,
+    courses,
+    pagination
+  }
+}
