50ded4228b0e38d96ab4c212c8dee3fe344588f7
yoelvp <yoelvp73@gmail.com>
2024-11-04 00:56:07 -0500

    feat: add pagination to store

--- src/modules/courses/store/published-courses.store.ts
+++ src/modules/courses/store/published-courses.store.ts
@@ -1,13 +1,19 @@
 import type { PublishedCourse } from '../types/Course'
+import type { Pagination } from '@/@common/types/Pagination'
 
 import { create } from 'zustand'
+import { DEFAULT_PAGINATION } from '@/@common/constants/default-pagination'
 
 interface UsePublishedCoursesStore {
   courses: PublishedCourse[] | null
+  pagination: Pagination
   setCourses: (courses: PublishedCourse[] | null) => void
+  setPagination: (courses: Pagination) => void
 }
 
 export const usePublishedCoursesStore = create<UsePublishedCoursesStore>()((set) => ({
   courses: null,
-  setCourses: (courses: PublishedCourse[] | null) => set({ courses })
+  pagination: DEFAULT_PAGINATION,
+  setCourses: (courses: PublishedCourse[] | null) => set({ courses }),
+  setPagination: (pagination: Pagination) => set({ pagination })
 }))
