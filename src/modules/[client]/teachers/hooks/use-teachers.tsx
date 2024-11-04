a5455026b8918369826e488e51be6458929c08a8
yoelvp <yoelvp73@gmail.com>
2024-11-04 01:33:53 -0500

    refactor: update hook and add paginaiton

--- src/modules/[client]/teachers/hooks/use-teachers.tsx
+++ src/modules/[client]/teachers/hooks/use-teachers.tsx
@@ -1,32 +1,48 @@
 import type { Teacher } from '@/_models/Teacher.model'
+import type { Pagination } from '@/@common/types/Pagination'
 
 import { useEffect, useState } from 'react'
-import { useLoading } from '@/@common/hooks/use-loading'
 import { toast } from 'sonner'
+import { HttpStatusCode } from 'axios'
+import { useLoading } from '@/@common/hooks/use-loading'
+import { getAllTeachersService } from '@/_services/teachers-client.service'
+import { DEFAULT_PAGINATION } from '@/@common/constants/default-pagination'
+import { usePagination } from '@/@common/hooks/use-pagination'
+import { responseMapper } from '@/@common/utils/response-mapper'
 import getError from '@/@common/utils/get-errors'
-import { getAllTeachersService } from '@/_services/teachers.service'
 
-export const useTeachers = (page: number, size: number) => {
+export const useTeachers = () => {
   const [teachers, setTeachers] = useState<Teacher[] | null>(null)
+  const [paginationState, setPaginationState] = useState<Pagination>(DEFAULT_PAGINATION)
+  const pagination = usePagination(paginationState)
   const { isLoading, loaded, loading } = useLoading()
 
   useEffect(() => {
-    const fetchTeachers = async () => {
-      try {
-        loading()
-        const { data } = await getAllTeachersService(page, size)
+    getAllTeachers()
+  }, [pagination.page, pagination.size])
+
+  const getAllTeachers = async () => {
+    try {
+      loading()
+      const { data: resData, status } = await getAllTeachersService({ ...pagination })
+
+      if (status === HttpStatusCode.Ok) {
+        const data = responseMapper(resData)
         setTeachers(data.results)
-      } catch (error) {
-        loaded()
-        const { message } = getError(error)
-        toast.error(message)
-      } finally {
-        loaded()
+        setPaginationState(data)
       }
+    } catch (error) {
+      loaded()
+      const { message } = getError(error)
+      toast.error(message)
+    } finally {
+      loaded()
     }
+  }
 
-    fetchTeachers()
-  }, [page, size])
-
-  return { teachers, isLoading }
+  return {
+    isLoading,
+    teachers,
+    pagination
+  }
 }
