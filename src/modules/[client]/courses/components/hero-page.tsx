51b063ce6a28af49e513f21653c5f2a250f95cf9
yoelvp <yoelvp73@gmail.com>
2024-11-03 19:29:11 -0500

    fix: update course props in pcomponents

--- src/modules/[client]/courses/components/hero-page.tsx
+++ src/modules/[client]/courses/components/hero-page.tsx
@@ -2,61 +2,64 @@ import { RenderHTML } from '@/@common/components'
 import { RatingCourse } from './rating'
 import { ContentFull } from '@/@common/components/content-full'
 import { Link } from 'react-router-dom'
+import { CourseDetails } from '@/_models/Course.model'
 
-export const HeroPage = ({ course }) => {
+interface Props {
+  course: CourseDetails | null
+}
+
+export const HeroPage = ({ course }: Props) => {
   return (
     <ContentFull
       containerClassName="h-auto bg-gradient-to-r from-primary-500 to-primary-400 mb-8 md:mb-0"
       contentClassName="flex-between flex-col-reverse gap-y-4 pt-16 pb-16 md:flex-row md:gap-x-32"
     >
       <section className="flex w-full sm:w-6/12  flex-col gap-8">
-        <article className="flex gap-2 text-primary-100">
-          <Link to="/" className="hover:text-white ease-in-out">
-            Derecho
-          </Link>
-          <span>&gt;</span>
-          <Link to="#" className="hover:text-white ease-in-out">
-            Derecho Administrativo
-          </Link>
-        </article>
+        {/* <article className="flex gap-2 text-primary-100"> */}
+        {/*   <Link to="/" className="hover:text-white ease-in-out"> */}
+        {/*     Derecho */}
+        {/*   </Link> */}
+        {/*   <span>&gt;</span> */}
+        {/*   <Link to="#" className="hover:text-white ease-in-out"> */}
+        {/*     Derecho Administrativo */}
+        {/*   </Link> */}
+        {/* </article> */}
 
         <article className="grid gap-2">
           <h1 className="text-white text-2xl font-semibold">{course?.name}</h1>
           <div className="text-primary-50">
-            {course?.objective ? (
-              <RenderHTML content={course?.objective} />
-            ) : (
-              <p>No se ha proporcionado un objetivo para este curso.</p>
-            )}
+            {course?.objective}
           </div>
         </article>
 
         <article className="flex flex-col gap-y-4">
-          <div className="flex flex-col w-full justify-start sm:flex-row sm:gap-x-4 sm:items-center">
-            <span className="text-sm font-semibold text-primary-200">
-              INSTRUCTOR
-            </span>
-            <div className="flex justify-start items-center gap-x-2">
-              <Link
-                to="/teachers/nancy-lozano-diaz"
-                className="text-white underline"
-              >
-                {course?.teacher.firstName} {course?.teacher.lastName}
-              </Link>
-              <div className="w-auto rounded-lg bg-secondary-500 px-2 text-xs">
-                {course?.teacher.profession}
+          {course?.teacher && (
+            <div className="flex flex-col w-full justify-start sm:flex-row sm:gap-x-4 sm:items-center">
+              <span className="text-sm font-semibold text-primary-200">
+                INSTRUCTOR
+              </span>
+              <div className="flex justify-start items-center gap-x-2">
+                <Link
+                  to="/teachers/nancy-lozano-diaz"
+                  className="text-white underline"
+                >
+                  {course?.teacher?.firstName} {course?.teacher?.lastName}
+                </Link>
+                <div className="w-auto rounded-lg bg-secondary-500 px-2 text-xs">
+                  {course?.teacher?.profession}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex w-full flex-col sm:flex-row sm:gap-x-2">
-            <div className="flex items-center gap-x-2">
-              <span className="text-secondary-500 font-semibold">4.0</span>
-              <RatingCourse count={5} filled={4} />
-            </div>
-            <span className="text-primary-100">
-              (120 calificaciones) • 176 alumnos
-            </span>
-          </div>
+          )}
+          {/* <div className="flex w-full flex-col sm:flex-row sm:gap-x-2"> */}
+          {/*   <div className="flex items-center gap-x-2"> */}
+          {/*     <span className="text-secondary-500 font-semibold">4.0</span> */}
+          {/*     <RatingCourse count={5} filled={4} /> */}
+          {/*   </div> */}
+          {/*   <span className="text-primary-100"> */}
+          {/*     (120 calificaciones) • 176 alumnos */}
+          {/*   </span> */}
+          {/* </div> */}
         </article>
       </section>
 
--- src/modules/[client]/courses/components/more-courses-by-teacher.tsx
+++ src/modules/[client]/courses/components/more-courses-by-teacher.tsx
@@ -1,14 +1,13 @@
-import type { FC } from 'react'
+/* import { CourseDetails } from '@/_models/Course.model' */
+/* import { CardCourseByTeacher } from './card-course-by-teacher' */
 
-import { CardCourseByTeacher } from './card-course-by-teacher'
-
-export const MoreCoursesByTeacher: FC = () => {
+export const MoreCoursesByTeacher = () => {
   return (
     <div className="grid gap-4">
       <h4 className="text-primary-700 font-bold text-2xl">Más cursos de Nancy Lozano Diaz</h4>
       <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2">
-        <CardCourseByTeacher />
-        <CardCourseByTeacher />
+        {/* <CardCourseByTeacher /> */}
+        {/* <CardCourseByTeacher /> */}
       </div>
     </div>
   )
