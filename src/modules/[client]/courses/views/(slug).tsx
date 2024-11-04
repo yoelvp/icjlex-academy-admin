00e57e379c27d58eb6417d4e8ab23ee9b3a43b74
yoelvp <yoelvp73@gmail.com>
2024-11-04 16:32:03 -0500

    fix: validate coursewhen user paid

--- src/modules/[client]/courses/views/(slug).tsx
+++ src/modules/[client]/courses/views/(slug).tsx
@@ -8,11 +8,14 @@ import { ContentDescription } from '../components/content-description'
 import { CoursesValorations } from '../components/courses-valorations'
 import { MoreCoursesByTeacher } from '../components/more-courses-by-teacher'
 import { FooterToBuy } from '../components/footer-to-buy'
-import { useGetCourseDetails } from '../hooks'
+import { useGetCheckStudentPaidCourse, useGetCourseDetails } from '../hooks'
 import { CoursePlaceholder } from '../components/course-placeholder'
+import classNames from 'classnames'
+import { IconRadioButtonOff } from '@/assets/icons'
 
 const CourseDetailsPage = () => {
   const { isLoading, course } = useGetCourseDetails()
+  const { paidCourse } = useGetCheckStudentPaidCourse()
 
   if (isLoading) return <CoursePlaceholder />
 
@@ -21,18 +24,59 @@ const CourseDetailsPage = () => {
       <HeroPage course={course} />
 
       <Content className="flex flex-col items-start mt-16 mb-24 gap-y-16 md:mt-32 md:mb-40 xl:gap-x-32 lg:gap-x-16 lg:flex-row-reverse">
-        <DetailsCourseByPurchased course={course} />
+        {paidCourse && !paidCourse?.isPaid && (
+          <DetailsCourseByPurchased course={course} />
+        )}
 
         <div className="w-full flex flex-col gap-y-16">
-          <WillLearn course={course} />
-          <ContentVariousVideos course={course} />
+          <div
+            className={classNames(
+              'flex flex-col gap-y-16',
+              { 'flex-col-reverse': paidCourse?.isPaid }
+            )}
+          >
+            {paidCourse?.isPaid && (
+              <div className="flex flex-col gap-4 w-full">
+                <span className="text-primary-700 font-semibold">
+                  El curso incluye:{' '}
+                </span>
+                {course?.includes ? (
+                  <div className="flex flex-col gap-2">
+                    {Array.isArray(course.includes) ? course.includes.map((item, index) => (
+                      <span
+                        className="text-primary-500 flex gap-4 items-center"
+                        key={index}
+                      >
+                        <IconRadioButtonOff size={16} className="text-primary-400" />
+                        <p className="text-primary-500">{item}</p>
+                      </span>
+                    )) : (
+                      <span className="text-primary-500 flex gap-4 items-center">
+                        <IconRadioButtonOff size={16} className="text-primary-400" />
+                        <p className="text-primary-500">{course.includes}</p>
+                      </span>
+                    )}
+                  </div>
+                ) : (
+                  <span className="text-primary-500 flex gap-4 items-center">
+                    <IconRadioButtonOff size={16} className="text-primary-400" />
+                    <p className="text-primary-500">No hay recursos incluidos</p>
+                  </span>
+                )}
+              </div>
+            )}
+            <WillLearn course={course} />
+            <ContentVariousVideos course={course} paidCourse={paidCourse} />
+          </div>
           <ContentDescription description={course?.description ?? ''} />
           <CoursesValorations />
           <MoreCoursesByTeacher />
         </div>
       </Content>
 
-      <FooterToBuy price={course?.price} name={course?.name} />
+      {paidCourse && !paidCourse?.isPaid && (
+        <FooterToBuy price={course?.price} name={course?.name} />
+      )}
     </Fragment>
   )
 }
