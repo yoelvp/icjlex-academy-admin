8991266e24f7c1f1c6b9523b582f27948b17ef81
yoelvp <yoelvp73@gmail.com>
2024-11-04 19:32:44 -0500

    fix: udapte check

--- src/@auth/components/check-auth.tsx
+++ src/@auth/components/check-auth.tsx
@@ -16,13 +16,13 @@ export const CheckAuth: FC<Props> = ({
   const token = useTokenStore((state) => state.token)
   const isAdmin = useCanStore((state) => state.isAdmin)
   const role = useCanStore((state) => state.role)
-  const permissions = useCanStore((state) => state.permissions)
+  const permission = useCanStore((state) => state.permission)
 
   if (!user && !token) return <Navigate to="/auth/login" replace />
   if (
     !isAdmin
     || !role
-    || !permissions
+    || !permission
   ) return <Navigate to="/" replace />
 
   return children
