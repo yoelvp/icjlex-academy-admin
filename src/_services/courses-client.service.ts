3c71acd336328341857efc0f26bc744ac8bcdb04
yoelvp <yoelvp73@gmail.com>
2024-11-14 11:55:07 -0500

    feat: add new service

--- src/_services/courses-client.service.ts
+++ src/_services/courses-client.service.ts
@@ -1,6 +1,14 @@
-import type { CourseDetails } from '@/_models/Course.model'
+import type { ResponsePaginated } from '@/@common/types/ResponseData'
+import type { Course, CourseDetails } from '@/_models/Course.model'
+
 import { axios } from '@/lib'
 
 export const getCourseDetailsService = (courseId: string) => {
   return axios.get<CourseDetails>(`/courses/${courseId}/details`)
+}
+
+export const getAllUpcomingCoursesService = (params?: object) => {
+  return axios.get<ResponsePaginated<Course>>('/c/courses', {
+    params
+  })
 }
