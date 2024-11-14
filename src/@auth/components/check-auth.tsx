0049b7744ac76775870077aa26ef6d14b8f76569
yoelvp <yoelvp73@gmail.com>
2024-11-14 11:53:35 -0500

    fix: change validation

--- src/@auth/components/check-auth.tsx
+++ src/@auth/components/check-auth.tsx
@@ -4,6 +4,7 @@ import { Navigate } from 'react-router-dom'
 import { useUserStore } from '../store/use-user.store'
 import { useTokenStore } from '../store/use-token.store'
 import { useCanStore } from '../store/use-can.store'
+import { ROLES } from '../utils/roles'
 
 interface Props {
   children: ReactNode
@@ -15,15 +16,11 @@ export const CheckAuth: FC<Props> = ({
   const user = useUserStore((state) => state.user)
   const token = useTokenStore((state) => state.token)
   const isAdmin = useCanStore((state) => state.isAdmin)
-  const role = useCanStore((state) => state.role)
-  const permission = useCanStore((state) => state.permission)
+  const roles = useCanStore((state) => state.roles)
+  const permissions = useCanStore((state) => state.permissions)
 
   if (!user && !token) return <Navigate to="/auth/login" replace />
-  if (
-    !isAdmin
-    || !role
-    || !permission
-  ) return <Navigate to="/" replace />
+  if (!isAdmin || !roles?.includes(ROLES.ADMIN) || !permissions?.length) return <Navigate to="/" replace />
 
   return children
 }
