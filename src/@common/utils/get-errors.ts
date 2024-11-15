0858ddc9b40934ca09311ce4a64cb4b63f566c11
yoelvp <yoelvp73@gmail.com>
2024-11-15 11:31:31 -0500

    fix: add type for error and style code

--- src/@common/utils/get-errors.ts
+++ src/@common/utils/get-errors.ts
@@ -1,28 +1,32 @@
-const getError = (error: any) => {
+import { AxiosError } from 'axios'
+
+const getError = (error: AxiosError<{ message?: string }>) => {
   switch (typeof error) {
     case 'object':
       if (error.response) {
         if (error.response.data) {
-          const message = error.response.data?.message ?? 'Ocurrió un error';
+          const message = error.response.data?.message ?? 'Ocurrió un error'
+
           return { message, status: error.response.status };
         }
       } else {
         if (!navigator.onLine) {
           return {
-            message: 'No tienes conexión a internet, Conectate a internet para continuar.',
+            message: 'No tienes conexión a internet, Conectate a internet para continuar.'
           };
         }
         if (error.message == 'Network Error') return { message: 'No se ha podido conectar con el servidor' }
+
         return { message: error.message };
       }
       break;
-    case "string":
-      return { message: `Ocurrio un error: ${error}` };
+    case 'string':
+      return { message: `Ocurrio un error: ${error}` }
     default:
-      return { message: `Ocurrio un error` }
+      return { message: 'Ocurrio un error' }
   }
 
-  return { message: `Ocurrio un error` }
+  return { message: 'Ocurrio un error' }
 }
 
 export default getError
