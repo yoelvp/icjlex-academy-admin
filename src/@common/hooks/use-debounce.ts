f6c2decaac50f170eb29e186f5ca0d81bbe73773
yoelvp <yoelvp73@gmail.com>
2024-11-19 13:06:16 -0500

    feat: add new hook of debounce

---
+++ src/@common/hooks/use-debounce.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+
+interface UseDebounce<T> {
+  value: T
+  delay?: number
+}
+
+export const useDebounce = <T>({
+  value,
+  delay = 1000
+}: UseDebounce<T>) => {
+  const [debouncedQuery, setDebouncedQuery] = useState(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedQuery(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(timer)
+    };
+  }, [value, delay])
+
+  return debouncedQuery
+}
