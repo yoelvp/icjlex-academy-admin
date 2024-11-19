e37ae8400daaf70e7d0beda1fa1b0ec21927311e
yoelvp <yoelvp73@gmail.com>
2024-11-19 13:07:30 -0500

    feat: add hook for delete teacher

---
+++ src/modules/teachers/hooks/use-delete-teacher.ts
@@ -0,0 +1,36 @@
+import { useLoading } from '@/@common/hooks'
+import { useTeacherStore } from '../store'
+import { HttpStatusCode, isAxiosError } from 'axios'
+import getError from '@/@common/utils/get-errors'
+import { toast } from 'sonner'
+import { deleteTeacherService } from '@/_services/teachers.service'
+
+export const useDeleteTeacher = () => {
+  const { isLoading, loading, loaded } = useLoading()
+  const teachers = useTeacherStore((state) => state.teachers)
+  const setTeachers = useTeacherStore((state) => state.setTeachers)
+
+  const deleteTeacher = async (teacherId: string) => {
+    loading()
+    try {
+      const { data: { data }, status } = await deleteTeacherService(teacherId)
+      if (status === HttpStatusCode.Ok) {
+        const newTeachers = teachers.filter((teacher) => teacher.id !== data.teacherId)
+        setTeachers(newTeachers)
+      }
+    } catch (error) {
+      loaded()
+      if (isAxiosError(error)) {
+        const { message } = getError(error)
+        toast.error(message)
+      }
+    } finally {
+      loaded()
+    }
+  }
+
+  return {
+    isLoading,
+    deleteTeacher
+  }
+}
