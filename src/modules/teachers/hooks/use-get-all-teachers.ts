e3fd80a991bcf9f58c756ba2275e3e5a3e9779e1
yoelvp <yoelvp73@gmail.com>
2024-11-19 13:07:50 -0500

    fix: add search query for teachers

--- src/modules/teachers/hooks/use-get-all-teachers.ts
+++ src/modules/teachers/hooks/use-get-all-teachers.ts
@@ -1,32 +1,42 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import { useTeacherStore } from '../store/teachers.store'
 import getError from '@/@common/utils/get-errors'
 import { useLoading } from '@/@common/hooks/use-loading'
 import { getAllTeachersService } from '@/_services/teachers.service'
 import { AxiosError } from 'axios'
-import { usePagination } from '@/@common/hooks'
+import { useDebounce, usePagination } from '@/@common/hooks'
 
 export const useGetAllTeachers = () => {
+  const [searchQuery, setSearchQuery] = useState('')
   const { isLoading, loading, loaded } = useLoading()
   const setTeachers = useTeacherStore((state) => state.setTeachers)
   const teacherPagination = useTeacherStore((state) => state.pagination)
   const setPagination = useTeacherStore((state) => state.setPagination)
   const paginationManager = usePagination(teacherPagination)
+  const debounceValue = useDebounce({ value: searchQuery })
 
   useEffect(() => {
     fetchTeachers()
-  }, [paginationManager.page, paginationManager.perPage])
+  }, [debounceValue, paginationManager.page, paginationManager.perPage])
 
   const fetchTeachers = async () => {
     loading()
     try {
       const { data: { data, pagination } } = await getAllTeachersService({
         page: paginationManager.page,
-        perPage: paginationManager.perPage
+        perPage: paginationManager.perPage,
+        q: searchQuery
       })
       setTeachers(data)
-      setPagination(pagination)
+      if (data.length > 0) {
+        setPagination(pagination)
+      } else {
+        setPagination({
+          ...pagination,
+          page: 1
+        })
+      }
     } catch (error) {
       if (error instanceof AxiosError) {
         const { message } = getError(error)
@@ -37,8 +47,13 @@ export const useGetAllTeachers = () => {
     }
   }
 
+  const updateSearchQuery = (value: string) => {
+    setSearchQuery(value)
+  }
+
   return {
     isLoading,
-    pagination: paginationManager
+    pagination: paginationManager,
+    search: updateSearchQuery
   }
 }
