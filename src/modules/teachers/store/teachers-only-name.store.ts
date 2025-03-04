338ff9f12032671484612592721e4c91d8030826
yoelvp <yoelvp73@gmail.com>
2025-03-04 12:05:29 -0500

    feat: add store for main data of teacher

---
+++ src/modules/teachers/store/teachers-only-name.store.ts
@@ -0,0 +1,12 @@
+import { create } from 'zustand'
+import { TeacherOnlyNames } from '@/_models/Teacher'
+
+interface UseTeachersOnlyNamesStore {
+  teachers: TeacherOnlyNames[]
+  setTeachers: (teacher: TeacherOnlyNames[]) => void
+}
+
+export const useTeachersOnlyNamesStore = create<UseTeachersOnlyNamesStore>()((set) => ({
+  teachers: [],
+  setTeachers: (teachers: TeacherOnlyNames[]) => set({ teachers })
+}))
