356c0c9134950a52e77717b1da41b86ed62e5e94
yoelvp <yoelvp73@gmail.com>
2025-03-04 22:17:20 -0500

    feat: add hook for publish course

---
+++ src/modules/courses/hooks/use-publish-course.ts
@@ -0,0 +1,35 @@
+import { HttpStatusCode, isAxiosError } from 'axios'
+import { toast } from 'sonner'
+import { useNavigate } from 'react-router-dom'
+import { useLoading } from '@/@common/hooks'
+import getError from '@/@common/utils/get-errors'
+import { publishCourseService } from '@/_services/admin/courses.service'
+
+export const usePublishCourse = () => {
+  const navigate = useNavigate()
+  const { isLoading, loading, loaded } = useLoading()
+
+  const publishCourse = async (courseId: string) => {
+    loading()
+    try {
+      const { status } = await publishCourseService(courseId)
+
+      if (status === HttpStatusCode.Ok) {
+        navigate('/admin/courses/')
+      }
+    } catch (error) {
+      loaded()
+      if (isAxiosError(error)) {
+        const { message } = getError(error)
+        toast.error(message)
+      }
+    } finally {
+      loaded()
+    }
+  }
+
+  return {
+    isLoading,
+    publishCourse
+  }
+}
