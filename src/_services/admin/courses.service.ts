9d42cb2eb3a050a39a3a06daefdd6b9efb638700
yoelvp <yoelvp73@gmail.com>
2025-03-04 22:18:48 -0500

    fix: add new services dor courses

--- src/_services/admin/courses.service.ts
+++ src/_services/admin/courses.service.ts
@@ -1,21 +1,21 @@
-import { API_URL } from '@/@common/env'
-import { Response } from '@/@common/types/Response'
-import { ResponseData } from '@/@common/types/ResponseData'
-import { Course, CourseListAdmin } from '@/_models/Course.model'
+import type { AdminCourse, Course } from '@/_models/Course.model'
+
 import { axios } from '@/lib'
-import { CourseInfomation, CourseResult } from '@/modules/courses/types/Course'
+import { API_URL } from '@/@common/env'
+import { IdAndNameFields } from '@/@common/types/IdAndName'
+import { Response } from '@/@common/types/Response'
 import { CourseFormData } from '@/modules/courses/types/CourseFormFields'
 
 export const createCourseService = (course: CourseFormData) => {
   const formData = new FormData()
   formData.append('name', course.name)
-  formData.append('docentId', course.teacherId)
+  formData.append('teacherId', course.teacherId)
   formData.append('objective', course.objective)
   formData.append('description', course.description)
   formData.append('isScheduled', `${course.isScheduled}`)
   formData.append('isFree', `${course.isFree}`)
   formData.append('price', `${course.price}`)
-  formData.append('publicationDate', `${course.publicationDate}`)
+  formData.append('publicationDate', `${course.publicationDate?.toISOString().slice(0, 19).replace('T', ' ') ?? null}`)
   formData.append('courseName', course.course.name)
   formData.append('courseUrl', `${course.course.url}`)
   formData.append('courseDuration', course.course.duration)
@@ -30,20 +30,28 @@ export const createCourseService = (course: CourseFormData) => {
     formData.append('image', course.image)
   }
 
-  return axios.post<CourseResult>('/admin/courses', formData, {
+  return axios.post<AdminCourse>('/admin/courses', formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
   })
 }
 
-// Not used
 export const getCourseByIdService = (courseId: string) => {
-  return axios.get<Course>(`/courses/${courseId}`)
+  return axios.get<Course>(`/admin/courses/${courseId}`)
+}
+
+// Not used
+export const getAllCoursesOnlyNameService = () => {
+  return axios.get<IdAndNameFields[]>('/courses/only-name')
+}
+
+export const publishCourseService = (courseId: string) => {
+  return axios.patch<Response<boolean>>(`/admin/courses/publish/${courseId}`)
 }
 
 export const getAllCoursesService = (params?: object) => {
-  return axios.get<Response<CourseListAdmin[]>>('/admin/courses', {
+  return axios.get<Response<AdminCourse[]>>('/admin/courses', {
     params
   })
 }
@@ -54,19 +62,6 @@ export const updateCourseService = async (
   courseData: Partial<Course>
 ) => {
   return await axios.patch(`${API_URL}/courses/${courseId}`, courseData)
-}
-
-// Not used
-export const getCoursesInformation = async (page: number, size: number) => {
-  return await axios.get<ResponseData<CourseInfomation>>(
-    `${API_URL}/courses/details`,
-    {
-      params: {
-        page,
-        size
-      }
-    }
-  )
 }
 
 export const deleteCourseService = async (courseId: string) => {
