787bfe547cc0c81b553c0131ebc0d99c31a1c376
yoelvp <yoelvp73@gmail.com>
2025-03-04 22:17:39 -0500

    feat: add hook for get course by id

---
+++ src/modules/courses/hooks/use-get-course-id.ts
@@ -0,0 +1,35 @@
+import { toast } from 'sonner'
+import { HttpStatusCode, isAxiosError } from 'axios'
+import { useLoading } from '@/@common/hooks/use-loading'
+import getError from '@/@common/utils/get-errors'
+import { getCourseByIdService } from '@/_services/admin/courses.service'
+import { useCourseStore } from '../store/course.store'
+
+export const useGetCourseById = () => {
+  const { isLoading, loading, loaded } = useLoading()
+  const setCourse = useCourseStore((state) => state.setCourse)
+
+  const getCourseById = async (courseId: string) => {
+    loading()
+    try {
+      const { data, status } = await getCourseByIdService(courseId)
+
+      if (status === HttpStatusCode.Ok) {
+        setCourse(data)
+      }
+    } catch (error) {
+      loaded()
+      if (isAxiosError(error)) {
+        const { message } = getError(error)
+        toast.error(message)
+      }
+    } finally {
+      loaded()
+    }
+  }
+
+  return {
+    isLoading,
+    getCourseById
+  }
+}
