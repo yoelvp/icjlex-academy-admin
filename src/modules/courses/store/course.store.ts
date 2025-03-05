b91989fab116ec08bb0d83c284e1c2576b12f03a
yoelvp <yoelvp73@gmail.com>
2025-03-04 22:17:02 -0500

    fix: change to store for one course

--- src/modules/courses/store/course.store.ts
+++ src/modules/courses/store/course.store.ts
@@ -1,23 +1,16 @@
 import { create } from 'zustand'
 import { CourseResult } from '../types/Course'
-import { Pagination } from '@/@common/types/Pagination'
-import { DEFAULT_PAGINATION } from '@/@common/constants/default-pagination'
 
 interface UseCourseStore {
   course: CourseResult | null
-  pagination: Pagination
   courseId: string | null
-  setCourses: (course: CourseResult[]) => void
-  setPagination: (pagination: Pagination) => void
+  setCourse: (course: CourseResult) => void
   setCourseId: (id: string | null) => void
 }
 
-export const UseCourseStore = create<UseCourseStore>((set) => ({
-  courses: null,
-  pagination: DEFAULT_PAGINATION,
+export const useCourseStore = create<UseCourseStore>((set) => ({
+  course: null,
   courseId: null,
-  setCourses: (courses: CourseResult[]) => set({ courses }),
-  setPagination: (pagination: Pagination) =>
-    set((state) => ({ ...state, pagination })),
-  setCourseId: (id: string | null) => set({ courseId: id })
+  setCourse: (course: CourseResult) => set({ course }),
+  setCourseId: (courseId: string | null) => set({ courseId })
 }))
