56e28775b9951c3bde6c732eb055b188eb02198f
yoelvp <yoelvp73@gmail.com>
2025-03-04 22:19:33 -0500

    fix: remove unused code

--- src/modules/[client]/teachers/hooks/use-get-course-by-teacher-id.ts
+++ src/modules/[client]/teachers/hooks/use-get-course-by-teacher-id.ts
@@ -2,14 +2,13 @@ import type { Course } from '@/_models/Course.model'
 
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
-import { getCoursesService } from '@/_services/courses.service'
 import { useLoading } from '@/@common/hooks/use-loading'
 import { usePagination } from '@/@common/hooks/use-pagination'
 import { Pagination } from '@/@common/types/Pagination'
 import { DEFAULT_PAGINATION } from '@/@common/constants/default-pagination'
-import { responseMapper } from '@/@common/utils/response-mapper'
 import getError from '@/@common/utils/get-errors'
 import { HttpStatusCode, isAxiosError } from 'axios'
+import { getAllCoursesService } from '@/_services/admin/courses.service'
 
 export const useGetCoursesByTeacherId = () => {
   const [courses, setCourses] = useState<Course[] | null>(null)
@@ -19,17 +18,16 @@ export const useGetCoursesByTeacherId = () => {
 
   useEffect(() => {
     getAllCourses()
-  }, [pagination.page, pagination.size])
+  }, [])
 
   const getAllCourses = async () => {
     try {
       loading()
-      const { data: resData, status } = await getCoursesService()
+      const { data: resData, status } = await getAllCoursesService()
 
       if (status === HttpStatusCode.Ok) {
-        const data = responseMapper(resData)
-        setCourses(data.results)
-        setPaginationState(data)
+        // setCourses(data.results)
+        // setPaginationState(data)
       }
     } catch (error) {
       loaded()
