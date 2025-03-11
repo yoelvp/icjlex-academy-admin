72d0018f768dbc476e484aba27e009ef8694bb31
yoelvp <yoelvp73@gmail.com>
2025-03-11 18:45:11 -0500

    fix: add padding in layout

--- src/layouts/auth/index.tsx
+++ src/layouts/auth/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router'
 
 export const AuthLayout = () => {
   return (
-    <div className="h-screen w-full grid place-content-center">
+    <div className="h-screen w-full grid place-content-center px-4">
       <Outlet />
     </div>
   )
