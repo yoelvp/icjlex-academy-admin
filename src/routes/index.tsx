1b2b92c821016364ab6d08637b1b3fa9477979e8
yoelvp <yoelvp73@gmail.com>
2025-03-11 17:00:03 -0500

    fix: clean routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,13 @@
 import { Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 
-import { ClientLayout } from '@/layouts/client'
+import { AuthLayout } from '@/layouts/auth'
 import { AdminLayout } from '@/layouts/admin'
 
-import HomePage from '@/modules/[client]/home/views'
-import TeachersPage from '@/modules/[client]/teachers/views'
-import TeacherDetailsPage from '@/modules/[client]/teachers/views/(slug)'
-import UsPage from '@/modules/[client]/us/views'
 import ChangePasswordPage from '@/modules/[auth]/change-password/views'
 import ForgotPasswordPage from '@/modules/[auth]/forgot-password/views'
 import RecoveryConfirmationPage from '@/modules/[auth]/forgot-password/views/recovery-confirmation'
 import LoginPage from '@/modules/[auth]/login/views'
-import CoursesPage from '@/modules/[client]/courses/views'
-import CourseDetailsPage from '@/modules/[client]/courses/views/(slug)'
 import { CheckAuth } from '@/@auth/components/check-auth'
 
 import {
@@ -27,64 +21,12 @@ import {
   CreateTeachersAdminPage,
   UpdateTeachersAdminPage
 } from './admin.routes'
-import CollaboratorDetailsPage from '@/modules/[client]/us/views/(slug)'
-import UpdateDataPage from '@/modules/[user]/update-data/views'
 
 const routes = createBrowserRouter([
-  // Client pages
-  {
-    element: <ClientLayout />,
-    errorElement: <div>error</div>,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />
-      },
-      {
-        path: '/us',
-        element: <UsPage />
-      },
-      {
-        path: '/us/:slug',
-        element: <CollaboratorDetailsPage />
-      },
-      {
-        path: '/teachers',
-        element: <TeachersPage />
-      },
-      {
-        path: '/teachers/:id',
-        element: <TeacherDetailsPage />
-      },
-      {
-        path: '/courses',
-        element: <CoursesPage />
-      },
-      {
-        path: '/courses/:slug/:id',
-        element: <CourseDetailsPage />
-      }
-    ]
-  },
-  // User routes
-  {
-    path: '/user',
-    element: <ClientLayout />,
-    children: [
-      {
-        path: 'update-data',
-        element: <UpdateDataPage />
-      },
-      {
-        path: 'courses',
-        element: <UpdateDataPage />
-      }
-    ]
-  },
   // Authentication pages
   {
     path: '/auth',
-    element: <ClientLayout />,
+    element: <AuthLayout />,
     children: [
       {
         path: 'login',
