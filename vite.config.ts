cb2b6818938122727d541be13d37f2e0d3f34daa
yoelvp <yoelvp73@gmail.com>
2025-03-11 17:54:42 -0500

    fix: change alias config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      '@': path.resolve(import.meta.dirname, './src')
-    }
+    alias: [
+      {
+        find: '@',
+        replacement: '/src'
+      }
+    ]
   }
 })
