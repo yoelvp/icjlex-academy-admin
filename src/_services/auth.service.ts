95aad2505be829f86cfa9d309846eb31d929bd2c
yoelvp <yoelvp73@gmail.com>
2025-03-11 18:44:56 -0500

    fix: add return type

--- src/_services/auth.service.ts
+++ src/_services/auth.service.ts
@@ -3,13 +3,9 @@ import { LoginData, LoginResponse } from '@/_models/Auth.model'
 import { axios } from '@/lib'
 
 export const loginService = (data: LoginData) => {
-  return axios.post<Response<LoginResponse>>('/admin/auth/login', data)
+  return axios.post<Response<LoginResponse>>('/auth/admin/login', data)
 }
 
 export const logoutService = () => {
-  return axios.post('/admin/auth/logout')
-}
-
-export const getRolesAndPermissionsByUserIdService = (userId: string) => {
-  return axios.get(`/admin/roles/role-and-permission/${userId}`)
+  return axios.post('/auth/admin/logout')
 }
