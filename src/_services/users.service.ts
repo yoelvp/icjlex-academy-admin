208afab3d6ba25da8ecdae58c01b402d85ef5865
yoelvp <yoelvp73@gmail.com>
2025-03-11 18:44:50 -0500

    feat: add new service

--- src/_services/users.service.ts
+++ src/_services/users.service.ts
@@ -1,5 +1,11 @@
+import { Response } from '@/@common/types/Response'
+import { RolesAndPermissionsResponse } from '@/_models/Auth.model'
 import { axios } from '@/lib'
 
 export const getUserByIdService = (userId: string) => {
   return axios.get(`/admin/users/${userId}`)
+}
+
+export const getRolesAndPermissionsByUserIdService = (userId: string) => {
+  return axios.get<Response<RolesAndPermissionsResponse>>(`/admin/users/roles-and-permissions/${userId}`)
 }
