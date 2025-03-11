f4f9b2a02cbcb2f91603f7bc31df6eb706285431
yoelvp <yoelvp73@gmail.com>
2025-03-11 17:00:22 -0500

    fix: change view layout and design

--- src/modules/[auth]/forgot-password/views/index.tsx
+++ src/modules/[auth]/forgot-password/views/index.tsx
@@ -2,7 +2,6 @@ import type { SubmitHandler } from 'react-hook-form'
 
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { Content } from '@/@common/components/content'
 import Form from '@/@common/components/form'
 import Button from '@/@common/components/button'
 import { recoverPasswordSchema } from '../schemas/recover-password.schema'
@@ -26,55 +25,47 @@ const ForgotPasswordPage = () => {
   }
 
   return (
-    <Content className="pt-32 pb-40 grid grid-cols-1 gap-y-16 md:grid-cols-3 md:gap-x-16 lg:grid-cols-2 lg:gap-x-32">
-      <section className="flex flex-col justify-center gap-y-8 md:col-span-2 lg:col-span-1">
-        <div>
-          <h1 className="text-primary-700 text-3xl text-center font-bold">
-            ¿Haz olvidado la contraseña?
-          </h1>
-          <p className="text-primary-400 text-center">
-            No te preocupes, ingresa tu correo electrónico registrado y te llegará un enlace para restablecer tu contraseña.
-          </p>
-        </div>
-        <div>
-          <Form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
-            <Form.Control>
-              <Form.Label>
-                Correo electrónico
-              </Form.Label>
-              <Form.Input
-                placeholder="Ingresa tu correo electrónico"
-                error={errors?.email?.message}
-                size="lg"
-                withIcon
-                icon={IconMail}
-                {...register('email')}
-              />
-              <Form.Error hasError={errors.email?.message} />
-            </Form.Control>
+    <section className="w-full max-w-lg border-2 border-primary-500/15 rounded-lg p-8 flex flex-col items-center justify-center gap-y-8">
+      <img src="/logo.svg" alt="logo with colors" className="h-16 w-auto" />
 
-            <Button htmlType="submit" disabled={!isDirty}>
-              Enviar enlace de recuperación
-            </Button>
-          </Form>
-        </div>
+      <div>
+        <h1 className="text-primary-700 text-3xl text-center font-bold">
+          ¿Haz olvidado la contraseña?
+        </h1>
+        <p className="text-primary-400 text-center">
+          No te preocupes, ingresa tu correo electrónico registrado y te llegará un enlace para restablecer tu contraseña.
+        </p>
+      </div>
+      <div className="w-full">
+        <Form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
+          <Form.Control>
+            <Form.Label>
+              Correo electrónico
+            </Form.Label>
+            <Form.Input
+              placeholder="Ingresa tu correo electrónico"
+              error={errors?.email?.message}
+              size="lg"
+              withIcon
+              icon={IconMail}
+              {...register('email')}
+            />
+            <Form.Error hasError={errors.email?.message} />
+          </Form.Control>
 
-        <div className="flex justify-center gap-x-2 text-primary-500">
-          Ó
-          <Link to="/auth/login" className="underline font-semibold hover:text-primary-700 hover:no-underline">
-            Inicia sesión
-          </Link>
-        </div>
-      </section>
+          <Button htmlType="submit" disabled={!isDirty}>
+            Enviar enlace de recuperación
+          </Button>
+        </Form>
+      </div>
 
-      <section className="h-full row-start-1 flex-col-center md:row-start-auto">
-        <img
-          src="/my-password.svg"
-          alt="masthead image"
-          className="w-3/5 h-auto md:w-full"
-        />
-      </section>
-    </Content>
+      <div className="flex justify-center gap-x-2 text-primary-500">
+        Ó
+        <Link to="/auth/login" className="underline font-semibold hover:text-primary-700 hover:no-underline">
+          Inicia sesión
+        </Link>
+      </div>
+    </section>
   )
 }
 
--- src/modules/[auth]/login/views/index.tsx
+++ src/modules/[auth]/login/views/index.tsx
@@ -6,11 +6,8 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { Spinner } from 'flowbite-react'
 import Button from '@/@common/components/button'
 import Form from '@/@common/components/form'
-import { Content } from '@/@common/components/content'
 import { IconLockCloseOutline, IconMail } from '@/assets/icons'
-import { whatsappMessage } from '@/@common/utils/whatsapp'
 import { useAuth } from '@/@auth/hooks/use-auth'
-import { APP_NAME_WITHOUT_AMP, WHATSAPP_ADMIN_NUMBER_PHONE } from '@/@common/env'
 import { LoginFormSchema } from '@/@auth/types/Login'
 import { loginFormSchema } from '@/@auth/schemas/login.schema'
 
@@ -29,86 +26,59 @@ const LoginPage = () => {
   }
 
   return (
-    <Content className="pt-32 pb-40 grid grid-cols-1 gap-y-16 md:grid-cols-3 md:gap-x-16 lg:grid-cols-2 lg:gap-x-32">
-      <section className="flex flex-col justify-center gap-y-8 md:col-span-2 lg:col-span-1">
-        <h1 className="text-primary-700 text-3xl text-center font-bold">
-          Inicia sesión en tu cuenta
-        </h1>
-        <div>
-          <Form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-y-4"
-          >
-            <Form.Control>
-              <Form.Label htmlFor="email">
-                Correo electrónico
-              </Form.Label>
-              <Form.Input
-                placeholder="Ingresa tu correo electrónico"
-                size="lg"
-                withIcon
-                icon={IconMail}
-                disabled={isLoading}
-                error={errors.email?.message}
-                {...register('email')}
-              />
-            </Form.Control>
-            <Form.Control>
-              <Form.Label htmlFor="password">
-                Contraseña
-              </Form.Label>
-              <Form.Password
-                placeholder="Ingresa tu contraseña"
-                size="lg"
-                withIcon
-                disabled={isLoading}
-                icon={IconLockCloseOutline}
-                error={errors.password?.message}
-                {...register('password')}
-              />
-            </Form.Control>
+    <section className="w-full max-w-lg border-2 border-primary-500/15 rounded-lg p-8 flex flex-col items-center justify-center gap-y-8">
+      <img src="/logo.svg" alt="logo with colors" className="h-16 w-auto" />
 
-            <Button htmlType="submit" disabled={isLoading}>
-              {isLoading && <Spinner color="success" />}
-              Iniciar sesión
-            </Button>
-          </Form>
-        </div>
+      <h1 className="text-primary-700 text-3xl text-center font-bold">
+        Inicia sesión en tu cuenta
+      </h1>
+      <div className="w-full">
+        <Form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-y-4"
+        >
+          <Form.Control>
+            <Form.Label htmlFor="email">
+              Correo electrónico
+            </Form.Label>
+            <Form.Input
+              placeholder="Ingresa tu correo electrónico"
+              size="lg"
+              withIcon
+              icon={IconMail}
+              disabled={isLoading}
+              error={errors.email?.message}
+              {...register('email')}
+            />
+          </Form.Control>
+          <Form.Control>
+            <Form.Label htmlFor="password">
+              Contraseña
+            </Form.Label>
+            <Form.Password
+              placeholder="Ingresa tu contraseña"
+              size="lg"
+              withIcon
+              disabled={isLoading}
+              icon={IconLockCloseOutline}
+              error={errors.password?.message}
+              {...register('password')}
+            />
+          </Form.Control>
 
-        <div className="flex justify-end">
-          <Link to="/auth/forgot-password" className="underline text-primary-500 hover:text-primary-700 hover:no-underline">
-            ¿Hás olvidado tu contraseña?
-          </Link>
-        </div>
+          <Button htmlType="submit" disabled={isLoading}>
+            {isLoading && <Spinner color="success" />}
+            Iniciar sesión
+          </Button>
+        </Form>
+      </div>
 
-        <hr className="sep-line-h" />
-
-        <div className="text-primary-500 flex justify-end gap-1">
-          <span>
-            ¿No tienes una cuenta?
-          </span>
-          <a
-            href={whatsappMessage({
-              message:`Quiero registrarme en ${APP_NAME_WITHOUT_AMP} y poder adquirir uno de sus cursos.`,
-              phoneNumber: WHATSAPP_ADMIN_NUMBER_PHONE
-            })}
-            className="text-primary-700 font-semibold underline hover:no-underline"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Registrate
-          </a>
-        </div>
-      </section>
-
-      <section className="h-full row-start-1 flex-col-center md:row-start-auto">
-        <img
-          src="/login.svg"
-          alt="masthead image"
-          className="w-3/5 h-auto md:w-full"
-        />
-      </section>
-    </Content>
+      <div className="flex justify-end">
+        <Link to="/auth/forgot-password" className="underline text-primary-500 hover:text-primary-700 hover:no-underline">
+          ¿Hás olvidado tu contraseña?
+        </Link>
+      </div>
+    </section>
   )
 }
 
