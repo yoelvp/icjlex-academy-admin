74aa2ee100ab1539ab16e2d3a27c9f41c101498b
yoelvp <yoelvp73@gmail.com>
2025-03-11 16:59:46 -0500

    fix: remove unnecesary modules

--- src/modules/[user]/update-data/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export { useValidateDataUpdate } from './use-validate-data-update'
-export { useUpdateData } from './use-update-data'
-export { useDropAcceptedFiles } from './use-drop-accepted-files'
--- src/modules/[user]/update-data/types/UpdateStudent.ts
+++
@@ -1,6 +0,0 @@
-import { InferType } from 'yup'
-import { updateDataSchema } from '../schemas/update-data.schema'
-
-export type UpdateStudent = InferType<typeof updateDataSchema> & {
-  image?: File
-}
