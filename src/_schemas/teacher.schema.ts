e8935186d34f6df0732140018c576821e0a1d88a
yoelvp <yoelvp73@gmail.com>
2025-03-11 22:59:27 -0500

    fix: change validation in image

--- src/_schemas/teacher.schema.ts
+++ src/_schemas/teacher.schema.ts
@@ -1,26 +1,35 @@
 import { array, mixed, object, string } from 'yup'
 
+const MAX_FILE_SIZE = 5 * 1024 * 1024
+const SUPPORTED_FORMATS = ['image/jpeg', 'image/png', 'image/webp'];
+
 export const teacherSchema = object({
-  firstName: string().required('El nombre es obligatorio.'),
-  lastName: string().required('El apellido es obligatorio.'),
+  firstName: string().required('Campo requerido'),
+  lastName: string().required('Campo requerido'),
   specialties: array()
     .of(object({
       label: string().required(),
       value: string().required()
     }))
     .required('Campo requerido')
-    .min(1, 'Ingrese al menos una especialidad'),
-  profession: string().required('La profesión es obligatoria.'),
-  about: string().required('El campo "Acerca de mí" es obligatorio.'),
-  imageUrl: string(),
-  image: mixed()
-    .test(
-      'file-type',
-      'Debe ingresar una imágen válida',
-      (value) => value instanceof File || value === null)
-    .required('Campo requerido'),
-  socialMedia: array()
-    .of(object({ url: string().required('Campo requerido') }))
+    .min(1, 'Ingrese al menos un item'),
+  profession: string().required('Campo requerido'),
+  about: string().required('Campo requerido'),
+  image: mixed<File>()
     .required('Campo requerido')
-    .min(1, 'Debe registrar al menos una red social del docente')
+    .test(
+      'fileSize',
+      'La imagen no debe exceder los 5 MB',
+      (value: File | null) => value ? value.size <= MAX_FILE_SIZE : false
+    )
+    .test(
+      'fileType',
+      'Debe ingresar una imágen válida',
+      (value: File | null) => value ? SUPPORTED_FORMATS.includes(value.type) : false
+    ),
+  imageUrl: string(),
+  socialMedia: array()
+    .of(string().required('Campo requerido'))
+    .required('Campo requerido')
+    .min(1, 'Debe agregar al menos un item')
 })
