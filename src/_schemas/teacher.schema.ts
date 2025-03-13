f9f3c79e13389deb1a79c933505c62357bda8e7d
yoelvp <yoelvp73@gmail.com>
2025-03-12 23:29:11 -0500

    fix: change image validation

--- src/_schemas/teacher.schema.ts
+++ src/_schemas/teacher.schema.ts
@@ -15,17 +15,29 @@ export const teacherSchema = object({
     .min(1, "Ingrese al menos un item"),
   profession: string().required("Campo requerido"),
   about: string().required("Campo requerido"),
-  image: mixed<File>()
+  image: mixed<FileList>()
     .required("Campo requerido")
     .test(
       "fileSize",
       "La imagen no debe exceder los 5 MB",
-      (value: File | null) => value ? value.size <= MAX_FILE_SIZE : false
+      (value) => {
+        if (!value || !(value instanceof FileList) || value.length === 0) {
+          return false
+        }
+
+        return value[0].size <= MAX_FILE_SIZE
+      }
     )
     .test(
       "fileType",
       "Debe ingresar una imágen válida",
-      (value: File | null) => value ? SUPPORTED_FORMATS.includes(value.type) : false
+      (value) => {
+        if (!value || !(value instanceof FileList) || value.length === 0) {
+          return false
+        }
+
+        return SUPPORTED_FORMATS.includes(value[0].type)
+      }
     ),
   imageUrl: string(),
   socialMedia: array()
