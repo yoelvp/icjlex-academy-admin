d28de9f99803e7a45b7390a353fb91c7068b1e38
yoelvp <yoelvp73@gmail.com>
2025-03-14 19:15:34 -0500

    fix: add config provider and dates Add provider of Ant Design and config for dayjs

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,26 @@
 import { RouterProvider } from "react-router"
 import { Toaster } from "sonner"
+import { ConfigProvider } from "antd";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import "dayjs/locale/es";
+import "@ant-design/v5-patch-for-react-19"
 import { routes } from "@/routes"
 import { AxiosInterceptor } from "./lib"
 import { ContainerConfirmModal } from "@/@common/components"
+import { theme } from "./lib/antd-theme";
 
 AxiosInterceptor()
 
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.locale("es")
+dayjs.tz.setDefault("America/Lima")
+
 const App = () => {
   return (
-    <>
+    <ConfigProvider theme={theme}>
       <ContainerConfirmModal />
       <Toaster
         richColors
@@ -17,7 +29,7 @@ const App = () => {
         visibleToasts={2}
       />
       <RouterProvider router={routes} />
-    </>
+    </ConfigProvider>
   )
 }
 
