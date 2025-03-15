5f7f5b41ba3f608f9650422c432d18856149233f
yoelvp <yoelvp73@gmail.com>
2025-03-14 19:19:02 -0500

    fix: change styles

--- src/@common/components/switch.tsx
+++ src/@common/components/switch.tsx
@@ -1,57 +1,51 @@
-import { forwardRef, type InputHTMLAttributes } from "react"
+import { ForwardedRef } from "react"
 import classNames from "classnames"
-import { motion } from "framer-motion"
+import { VariantProps } from "class-variance-authority"
+import { switchVariants } from "./+variants/switch.variant"
 
-interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, "value" | "onChange"> {
+type SwitchVariant = typeof switchVariants
+
+interface Props extends VariantProps<SwitchVariant> {
+  ref: ForwardedRef<HTMLInputElement>
   className?: string
   value?: boolean | null
-  onChange?: (checked: boolean) => void
+  onChange?: (value: boolean) => void
 }
 
-export const Switch = forwardRef<HTMLInputElement, Props>(({
-  className,
+export const Switch = ({
+  ref,
   value,
-  onChange,
-  ...props
-}, ref) => {
-  const toggleSwitch = () => {
-    onChange?.(!value)
-  }
-
+  className,
+  onChange
+}: Props) => {
   return (
-    <motion.button
+    <button
       type="button"
       className={classNames(
-        "relative inline-flex h-6 w-12 items-center rounded-full transition-colors border border-primary-100",
+        "relative inline-flex h-6 min-w-12 w-12 items-center rounded-full transition-colors border border-primary-100",
         "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background",
         "disabled:cursor-not-allowed disabled:opacity-50",
         value ? "bg-primary-100" : "bg-primary-50",
         className
       )}
-      onClick={toggleSwitch}
+      onClick={() => onChange?.(!value)}
       aria-checked={value ?? false}
       role="switch"
     >
-      <motion.span
+      <span
         className={classNames(
           "absolute inline-block h-5 w-5 rounded-full bg-primary-500 shadow-lg",
-          { "right-0": value }
+          { "right-0": value },
+          { "left-0": !value }
         )}
-        layout
-        transition={{
-          type: "spring",
-          stiffness: 700,
-          damping: 30
-        }}
       />
       <input
-        type="checkbox"
         ref={ref}
-        checked={value ?? false}
+        type="checkbox"
         className="sr-only"
-        onChange={(e) => onChange?.(e.target.checked)}
-        {...props}
+        checked={value ?? false}
+        onChange={(e) => onChange?.(!e.target.value)}
       />
-    </motion.button>
+    </button>
   )
-})
+}
