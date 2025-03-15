be0880bf3bc15acdd45d1d9aa63488e53daf337f
yoelvp <yoelvp73@gmail.com>
2025-03-14 19:18:32 -0500

    fix: change badge styles

--- src/@common/components/badge-optional.tsx
+++ src/@common/components/badge-optional.tsx
@@ -1,13 +1,14 @@
-import { Badge } from "flowbite-react"
+import classNames from "classnames"
 
 export const BadgeOptional = () => {
   return (
-    <Badge
-      color="gray"
-      size="sm"
-      className="font-thin text-xs"
+    <span
+      className={classNames(
+        "font-light text-xs bg-primary-50 text-primary-900 border border-zinc-300 rounded-sm",
+        "px-2"
+      )}
     >
-      opcional
-    </Badge>
+      Opcional
+    </span>
   )
 }
