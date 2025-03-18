ab0e1021c2e322c7cd757f2b08fb46eea88e73d6
yoelvp <yoelvp73@gmail.com>
2025-03-18 15:24:03 -0500

    fix: add more services

--- src/modules/teachers/hooks/use-create-teacher.ts
+++ src/modules/teachers/hooks/use-create-teacher.ts
@@ -2,18 +2,19 @@ import { toast } from "sonner"
 import getError from "@/@common/utils/get-errors"
 import { useLoading } from "@/@common/hooks/use-loading"
 import { HttpStatusCode, isAxiosError } from "axios"
-import { createTeacherService } from "@/_services/teachers.service"
-import { TeacherFormValues } from "@/_models/Teacher"
+import { createTeacherService, updateImageTeacherService } from "@/_services/teachers.service"
+import { TeacherFormValues } from "@/_models/Teacher.model"
 
 export const useCreateTeacher = () => {
   const { isLoading, loading, loaded } = useLoading()
 
-  const createTeacher = async (docent: TeacherFormValues) => {
+  const createTeacher = async (teacher: TeacherFormValues, image?: File | null) => {
     try {
       loading()
-      const { status } = await createTeacherService(docent)
+      const { data, status } = await createTeacherService(teacher)
+      const { status: updateImageTeacherStatus } = await updateImageTeacherService(data.data.teacherId ?? "", image)
 
-      if (status !== HttpStatusCode.Ok) return
+      if (status !== HttpStatusCode.Ok && updateImageTeacherStatus !== HttpStatusCode.Ok) return
 
       toast.success("Registro exitoso", { description: "El profesor fue registrado con éxito" })
     } catch (error) {
