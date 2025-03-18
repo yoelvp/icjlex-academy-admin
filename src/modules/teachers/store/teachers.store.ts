3709dc62fdcdce851172a09b89ccd7971aa04247
yoelvp <yoelvp73@gmail.com>
2025-03-18 15:23:46 -0500

    fix: remove teacher stor

--- src/modules/teachers/store/teachers.store.ts
+++ src/modules/teachers/store/teachers.store.ts
@@ -1,22 +1,18 @@
 import { create } from "zustand"
 import { Pagination } from "@/@common/types/Pagination"
 import { DEFAULT_PAGINATION } from "@/@common/constants/default-pagination"
-import { Teacher } from "@/_models/Teacher"
+import { Teacher } from "@/_models/Teacher.model"
 
-interface UseTeacherStore {
-  teacher: Teacher | null
+interface UseTeachersStore {
   teachers: Teacher[]
-  setTeacher: (teacher: Teacher | null) => void
   setTeachers: (teacher: Teacher[]) => void
   pagination: Pagination
   setPagination: (pagination: Pagination) => void
 }
 
-export const useTeacherStore = create<UseTeacherStore>()((set) => ({
-  teacher: null,
+export const useTeachersStore = create<UseTeachersStore>()((set) => ({
   teachers: [],
   pagination: DEFAULT_PAGINATION,
-  setTeacher: (teacher: Teacher | null) => set({ teacher }),
   setTeachers: (teachers: Teacher[]) => set({ teachers }),
   setPagination: (pagination: Pagination) => set({ pagination })
 }))
