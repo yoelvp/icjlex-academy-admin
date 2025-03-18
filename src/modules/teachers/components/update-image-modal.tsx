1536ae170816472701fbec7e88aae563502bb842
yoelvp <yoelvp73@gmail.com>
2025-03-18 15:25:18 -0500

    fix: change styles

--- src/modules/teachers/components/update-image-modal.tsx
+++ src/modules/teachers/components/update-image-modal.tsx
@@ -1,58 +1,67 @@
-import { Modal } from "@/@common/components/modal"
-import { useTeacherStore } from "../store/teachers.store"
-import { getFullName } from "@/@common/utils/get-full-names"
+import { useEffect, useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
-import { UpdateTeacherImage } from "../types/TeacherFormFields"
 import { yupResolver } from "@hookform/resolvers/yup"
-import { updateImageSchema } from "../schemas/update-image.schema"
-import ImageUploader from "@/modules/courses/components/image-uploader"
+import { Modal } from "@/@common/components/modal"
+import { ImageUpload } from "@/@common/components"
 import Button from "@/@common/components/button"
 import Form from "@/@common/components/form"
 import { useTeachers } from "../hooks/use-teachers"
+import { updateImageSchema } from "@/_schemas/teacher.schema"
+import { UpdateTeacherImageFormSchema } from "@/_models/Teacher.model"
 
 interface Props {
   show: boolean
   close: () => void
+  defaultImage?: string
+  teacherId: string
 }
 
 const UpdateImageModal = ({
   show,
-  close
+  close,
+  defaultImage,
+  teacherId
 }: Props) => {
+  const [isUpdate, setIsUpdate] = useState(false)
   const { isLoading, updateImage } = useTeachers()
-  const teacher = useTeacherStore((state) => state.teacher)
-  const setTeacher = useTeacherStore((state) => state.setTeacher)
-  const { setValue, handleSubmit, formState: { errors } } = useForm<UpdateTeacherImage>({
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<UpdateTeacherImageFormSchema>({
     resolver: yupResolver(updateImageSchema)
   })
 
-  const onHandleSubmit: SubmitHandler<UpdateTeacherImage> = (data) => {
-    updateImage(teacher?.id ?? "", data).then(() => {
-      close()
-      setTeacher(null)
-    })
+  const onHandleSubmit: SubmitHandler<UpdateTeacherImageFormSchema> = (data) => {
+    if (isUpdate) {
+      updateImage(teacherId, data.image?.[0]).then(() => {
+        close()
+      })
+    }
   }
 
   const handleUpdateImage = () => {
     handleSubmit(onHandleSubmit)()
   }
 
+  useEffect(() => {
+    setIsUpdate(true)
+
+    return () => setIsUpdate(false)
+  }, [watch("image")])
+
   return (
     <Modal
-      title={`Cambiar imagen | ${getFullName(teacher)}`}
+      title="Cambiar imagen"
       isOpen={show}
       onClose={close}
-      className="h-96"
+      className="h-auto"
     >
-      <div className="h-full grid grid-rows-[1fr_auto]">
+      <div className="h-full grid grid-rows-[1fr_auto] gap-y-8 pt-4">
         <div>
-          <ImageUploader name="image" setValue={setValue} />
+          <ImageUpload name="image" register={register} defaultImageUrl={defaultImage} />
           <Form.Error hasError={errors.image?.message} />
         </div>
 
         <div className="flex justify-end items-center gap-x-4">
           <Button
-            variant="error.outline"
+            variant="primary.outline"
             onClick={close}
           >
             Cancelar
