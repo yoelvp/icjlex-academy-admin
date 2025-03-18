43512d29bf2ecdf8878d26f0c3d2c75137b50d2b
yoelvp <yoelvp73@gmail.com>
2025-03-18 15:28:00 -0500

    fix: change rounded variant

--- src/@common/constants/button-variants.ts
+++ src/@common/constants/button-variants.ts
@@ -24,15 +24,11 @@ export const buttonIconVariants = cva(
     variants: {
       variant: ButtonStyle,
       size: {
-        sm: "h-6 w-6",
+        sm: "h-8 w-8",
         md: "h-10 w-10",
         lg: "h-12 w-12"
       },
-      rounded: {
-        sm: "rounded-[4px]",
-        md: "rounded-[6px]",
-        lg: "rounded-[8px]"
-      }
+      rounded: RoundedStyle
     },
     defaultVariants: {
       variant: "primary",
