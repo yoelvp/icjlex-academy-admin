ee3a98f50cc63a106b06a3c9778d0109d809887f
yoelvp <yoelvp73@gmail.com>
2025-03-18 15:24:42 -0500

    fix: add new service for updateImage

--- src/modules/teachers/hooks/use-teachers.ts
+++ src/modules/teachers/hooks/use-teachers.ts
@@ -3,18 +3,17 @@ import { HttpStatusCode, isAxiosError } from "axios"
 import { useLoading } from "@/@common/hooks/use-loading"
 import getError from "@/@common/utils/get-errors"
 import { updateImageTeacherService } from "@/_services/teachers.service"
-import { UpdateTeacherImage } from "../types/TeacherFormFields"
-import { useTeacherStore } from "../store/teachers.store"
+import { useTeachersStore } from "../store/teachers.store"
 
 export const useTeachers = () => {
   const { isLoading, loading, loaded } = useLoading()
-  const setTeachers = useTeacherStore((state) => state.setTeachers)
-  const teachers = useTeacherStore((state) => state.teachers)
+  const setTeachers = useTeachersStore((state) => state.setTeachers)
+  const teachers = useTeachersStore((state) => state.teachers)
 
-  const updateImage = async (teacherId: string, bodyData: UpdateTeacherImage) => {
+  const updateImage = async (teacherId: string, file?: File | null) => {
     try {
       loading()
-      const { data, status } = await updateImageTeacherService(teacherId, bodyData)
+      const { data, status } = await updateImageTeacherService(teacherId, file)
 
       if (status === HttpStatusCode.Ok) {
         const updatedTeachers = teachers.map((teacher) => teacher.id === data.teacherId ? { ...teacher, imageUrl: data.imageUrl ?? "" } : teacher)
