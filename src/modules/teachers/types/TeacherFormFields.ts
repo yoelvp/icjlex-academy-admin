d531594ef00dd324c529ee3c904735864ffe7bb9
yoelvp <yoelvp73@gmail.com>
2025-03-18 15:23:17 -0500

    fix: delete unused files or move another folder

--- src/modules/teachers/types/TeacherFormFields.ts
+++
@@ -1,5 +0,0 @@
-import type { InferType } from "yup"
-
-import { updateImageSchema } from "../schemas/update-image.schema"
-
-export type UpdateTeacherImage = InferType<typeof updateImageSchema>
--- src/modules/teachers/utils/active-tab.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum ActiveTabIndex {
-  ONE = 0,
-  TWO = 1,
-  TREE = 2,
-  FOUR = 3
-}
