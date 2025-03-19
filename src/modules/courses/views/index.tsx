6eddfdbe0eeddf7893a1e917e797994bf157000d
yoelvp <yoelvp73@gmail.com>
2025-03-18 21:33:15 -0500

    fix: add new states for edit and mo

--- src/modules/courses/views/index.tsx
+++ src/modules/courses/views/index.tsx
@@ -1,16 +1,42 @@
+import { ChangeEvent, Suspense, useState } from "react"
 import Form from "@/@common/components/form"
 import Link from "@/@common/components/link"
 import { Tabs, Tab } from "@/@common/components/tabs"
 import { useCourseUI } from "../hooks/use-courses-ui"
 import { TablePublishedCourses } from "../components/table-published-courses"
 import { TableScheduledCourses } from "../components/table-scheduled-courses"
+import { useShow } from "@/@common/hooks"
+import UpdateImageModal from "../components/update-image-modal"
+import { LoadingModal } from "@/@common/components"
+import { useGetCourses } from "../hooks/use-get-courses"
 import {
   IconAdd,
   IconSearch
 } from "@/assets/icons"
+import { TAB_INDEX } from "../utils/course-tab-index"
 
 const CoursesPage = () => {
+  const [courseId, setCourseId] = useState<string | null>(null)
+  const [courseImageUrl, setCourseImageUrl] = useState<string | null>(null)
   const { tab, handleTabIndex } = useCourseUI()
+  const { show: showUpdateImageModal, open: openUpdateImageModal, close: closeUpdateImageModal } = useShow()
+  const { search, searchUpcomingCourses } = useGetCourses()
+
+  const handleCourseId = (teacherId: string) => {
+    setCourseId(teacherId)
+  }
+
+  const handleCourseImageUrl = (imageUrl: string) => {
+    setCourseImageUrl(imageUrl)
+  }
+
+  const handleSearchCourse = (event: ChangeEvent<HTMLInputElement>) => {
+    if (tab === TAB_INDEX[0]) {
+      search(event.target.value)
+    } else {
+      searchUpcomingCourses(event.target.value)
+    }
+  }
 
   return (
     <div className="flex flex-col gap-y-4">
@@ -23,6 +49,7 @@ const CoursesPage = () => {
             placeholder="Buscar..."
             size="sm"
             rounded="sm"
+            onChange={handleSearchCourse}
             withIcon
             icon={IconSearch}
           />
@@ -36,13 +63,32 @@ const CoursesPage = () => {
       <section>
         <Tabs defaultValue={tab === null ? "published" : "upcoming"}>
           <Tab title="Publicados" value="published" onChange={() => handleTabIndex(0)}>
-            <TablePublishedCourses />
+            <TablePublishedCourses
+              handleCourseId={handleCourseId}
+              handleCourseImageUrl={handleCourseImageUrl}
+              openUpdateImageModal={openUpdateImageModal}
+            />
           </Tab>
           <Tab title="Programados" value="upcoming" onChange={() => handleTabIndex(1)}>
-            <TableScheduledCourses />
+            <TableScheduledCourses
+              handleCourseId={handleCourseId}
+              handleCourseImageUrl={handleCourseImageUrl}
+              openUpdateImageModal={openUpdateImageModal}
+            />
           </Tab>
         </Tabs>
       </section>
+
+      {showUpdateImageModal && (
+        <Suspense fallback={<LoadingModal />}>
+          <UpdateImageModal
+            show={showUpdateImageModal}
+            close={closeUpdateImageModal}
+            courseId={courseId ?? ""}
+            defaultImage={courseImageUrl ?? ""}
+          />
+        </Suspense>
+      )}
     </div>
   )
 }
