0337097e86e42332363b358d663024662604ca39
yoelvp <yoelvp73@gmail.com>
2025-04-10 15:10:07 -0500

    fix: clean hook

--- src/modules/students/hooks/use-get-all-students.ts
+++ src/modules/students/hooks/use-get-all-students.ts
@@ -1,60 +1,11 @@
-import { useEffect } from "react"
-import { getAllActiveStudentsService, getAllPreRegisteredStudentsService } from "../services/student.service"
-import getError from "@/@common/utils/get-errors"
-import { toast } from "sonner"
-import { useLoading } from "@/@common/hooks/use-loading"
-import { useStudentsStore } from "../store/use-students.store"
-import { QueryParams } from "@/@common/types/QueryParams"
-import { isAxiosError } from "axios"
+import { useQuery } from "@tanstack/react-query"
+import { getAllStudentsService } from "@/services/students.service"
+import { QueryKeys } from "@/@common/utils"
 
-export const useGetAllStudents = (activeParams: QueryParams, preRegisteredParams: QueryParams) => {
-  const { isLoading: isLoadingActive, loading: loadingActive, loaded: loadedActive } = useLoading()
-  const { isLoading: isLoadingPreRegister, loading: loadingPreRegistered, loaded: loadedPreRegistered } = useLoading()
-  const setActiveStudents = useStudentsStore((state) => state.setActiveStudents)
-  const setPreRegisteredStudents = useStudentsStore((state) => state.setPreRegisteredStudents)
-
-  useEffect(() => {
-    getAllActiveStudents()
-  }, [activeParams.page, activeParams.size, activeParams.query])
-
-  useEffect(() => {
-    getAllPreRegisteredStudents()
-  }, [preRegisteredParams.page, preRegisteredParams.size, preRegisteredParams.query])
-
-  const getAllActiveStudents = async () => {
-    try {
-      loadingActive()
-      const { data } = await getAllActiveStudentsService(activeParams)
-      setActiveStudents(data.results)
-    } catch (error) {
-      loadedActive()
-      if (isAxiosError(error)) {
-        const { message } = getError(error)
-        toast.error(message)
-      }
-    } finally {
-      loadedActive()
-    }
-  }
-
-  const getAllPreRegisteredStudents = async () => {
-    try {
-      loadingPreRegistered()
-      const { data } = await getAllPreRegisteredStudentsService(preRegisteredParams)
-      setPreRegisteredStudents(data.results)
-    } catch (error) {
-      loadedPreRegistered()
-      if (isAxiosError(error)) {
-        const { message } = getError(error)
-        toast.error(message)
-      }
-    } finally {
-      loadedPreRegistered()
-    }
-  }
-
-  return {
-    isLoadingActive,
-    isLoadingPreRegister
-  }
+export const useGetAllStudents = () => {
+  return useQuery({
+    queryKey: [QueryKeys.STUDENTS],
+    queryFn: getAllStudentsService,
+    select: (res) => res?.data
+  })
 }
