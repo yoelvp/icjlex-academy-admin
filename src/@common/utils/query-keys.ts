40e6ecd7da2323e6d7da6278caa6e50263e13065
yoelvp <yoelvp73@gmail.com>
2025-04-10 15:09:19 -0500

    fix: rename values

--- src/@common/utils/query-keys.ts
+++ src/@common/utils/query-keys.ts
@@ -1,8 +1,12 @@
 export const QueryKeys = {
-  COURSE_BY_ID: "courseById",
-  COURSE_TO_EDIT: "courseToEdit",
+  COURSE_BY_ID: "course-by-id",
+  COURSE_TO_EDIT: "course-to-edit",
 
   // Teachers
   TEACHERS: "teachers",
-  TEACHER_BY_ID: "teacherById"
+  TEACHER_BY_ID: "teacher-by-id",
+
+  // Teachers
+  STUDENTS: "students",
+  STUDENT_BY_ID: "teacher-by-id"
 }
