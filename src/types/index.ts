4ebd8320217321ce4f14358f5ca30b7fe99df9ba
yoelvp <yoelvp73@gmail.com>
2025-04-10 15:11:39 -0500

    feat: add index of auth

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+// Models
+export * from "./models/Auth"
+export * from "./models/Comment"
+export * from "./models/Comment"
+export * from "./models/Course"
+export * from "./models/Course"
+export * from "./models/Permission"
+export * from "./models/Role"
+export * from "./models/Student"
+export * from "./models/Teacher"
+export * from "./models/User"
+
+export * from "./common"
