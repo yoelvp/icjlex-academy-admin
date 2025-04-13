6991428de5accee4b0418366abfca9624d2bcd34
yoelvp <yoelvp73@gmail.com>
2025-04-12 23:25:55 -0500

    fix: change return value

--- src/@common/utils/get-initials.ts
+++ src/@common/utils/get-initials.ts
@@ -4,8 +4,8 @@ interface Payload {
 }
 
 export const getUserInitials = <T extends Payload>(user: T | null) => {
-  const initialFirstName = user?.firstName.charAt(0)
-  const initialLastName = user?.lastName.charAt(0)
+  const initialFirstName = user?.firstName?.charAt(0)
+  const initialLastName = user?.lastName?.charAt(0)
 
-  return `${initialFirstName}${initialLastName}`
+  return initialFirstName?.concat(initialLastName ?? "")
 }
