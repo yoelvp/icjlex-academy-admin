9882c3b64f4a3fd526b571a1ccac59294bc911cb
yoelvp <yoelvp73@gmail.com>
2025-04-12 23:26:11 -0500

    fix: change app name

--- src/@common/env.ts
+++ src/@common/env.ts
@@ -3,9 +3,9 @@ export const API_URL = import.meta.env.VITE_BASE_URL_API_V1
 export const JWT_SECRET_KEY = import.meta.env.VITE_JWT_SECRET_KEY
 
 // Utilities
-export const APP_NAME = "ICJ LEX & CARRANZA CONSULTORES"
-export const APP_NAME_WITHOUT_AMP = "ICJ LEX Y CARRANZA CONSULTORES"
-export const APP_SHORT_NAME= "ICJ LEX"
+export const APP_NAME = "ILECAC | Estudio Jurídico"
+export const APP_NAME_WITHOUT_AMP = "ILECAC | Estudio Jurídico | ICJ LEX Y CARRANZA CONSULTORES"
+export const APP_SHORT_NAME= "ILECAC"
 export const WHATSAPP_LINK = "https://wa.me/51942208501"
 export const WHATSAPP_BASE_URL = "https://wa.me"
 export const WHATSAPP_ADMIN_NUMBER_PHONE = "51915246869"
