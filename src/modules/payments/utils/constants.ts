ceacb1ed231901ce74d6594f2ab1449ff87ebe06
yoelvp <yoelvp73@gmail.com>
2025-05-30 17:51:36 -0500

    feat: add constants

---
+++ src/modules/payments/utils/constants.ts
@@ -0,0 +1,52 @@
+import { SelectOption } from "@/types"
+
+export const PAYMENT_TYPE: SelectOption[] = [
+  {
+    label: "Yape",
+    value: "YAPE"
+  },
+  {
+    label: "Plin",
+    value: "YAPE"
+  },
+  {
+    label: "Transferencia BCP",
+    value: "BCP"
+  },
+  {
+    label: "Transferencia Interbank",
+    value: "INTERBANK"
+  },
+  {
+    label: "Transferencia BBVA",
+    value: "BBVA"
+  },
+  {
+    label: "Transferencia Scotiabank",
+    value: "SCOTIABANK"
+  },
+  {
+    label: "Paypal",
+    value: "PAYPAL"
+  },
+  {
+    label: "Stripe",
+    value: "STRIPE"
+  },
+  {
+    label: "Mercado Pago",
+    value: "MERCADOPAGO"
+  },
+  {
+    label: "Pago Efectivo",
+    value: "PAGOEFECTIVO"
+  },
+  {
+    label: "Niubiz",
+    value: "NIUBIZ"
+  },
+  {
+    label: "Efectivo",
+    value: "Niubiz"
+  }
+]
